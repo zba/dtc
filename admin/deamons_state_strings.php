932a7d2e952bc7dcd04a4a85cb45cca61df36572
rroet <rroet>
2003-11-09 15:51:38 +0000

    Started filling in language support for daemon states.

--- admin/deamons_state_strings.php
+++ admin/deamons_state_strings.php
@@ -1,9 +1,46 @@
 <?php
 
+$txt_daemonstate_qmail_restart = array(
+	"fr" => "Qmail Restart",
+	"en" => "Qmail Restart",
+	"nl" => "Qmail herstarten",
+	"ru" => "Qmail Restart",
+	"de" => "Qmail Restart");
 
+$txt_daemonstate_qmail_newu = array(
+	"fr" => "Qmail New Users",
+	"en" => "Qmail New Users",
+	"nl" => "Nieuwe Qmail gebruikers",
+	"ru" => "Qmail New Users",
+	"de" => "Qmail New Users");
 
+$txt_daemonstate_gen_files = array(
+	"fr" => "Generate new Qmail userfile",
+	"en" => "Generate new Qmail userfile",
+	"nl" => "Genereer een nieuw Qmail gebruikersbestand",
+	"ru" => "Generate new Qmail userfile",
+	"de" => "Generate new Qmail userfile");
 
+$txt_daemonstate_apache_restart = array(
+	"fr" => "Apache Restart",
+	"en" => "Apache Restart",
+	"nl" => "Apache herstarten",
+	"ru" => "Apache Restart",
+	"de" => "Apache Restart");
 
+$txt_daemonstate_bind_reload = array(
+	"fr" => "Reload bind",
+	"en" => "Reload bind",
+	"nl" => "Bind herstarten",
+	"ru" => "Reload Bind",
+	"de" => "Reload Bind");
+
+$txt_daemonstate_next_cronjob = array(
+	"fr" => "Next Cronjob",
+	"en" => "Next cronjob",
+	"nl" => "Volgende cronjob",
+	"ru" => "Next cronjob",
+	"de" => "Next cronjob");
 
 
 ?>
