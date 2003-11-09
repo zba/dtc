4afb25ce9b623ae73b1564d4e84e27783f8ca276
thomas <thomas>
2003-11-09 15:30:14 +0000

    Added IF NOT EXSISTS statement so DTC can install over itself if already installed.

--- admin/tables/ftp_logs.sql
+++ admin/tables/ftp_logs.sql
@@ -1,17 +1,13 @@
-#
-# Table structure for table `ftp_logs`
-#
-
-CREATE TABLE `ftp_logs` (
-  `username` tinytext,
-  `filename` text,
-  `size` bigint(20) default NULL,
-  `host` tinytext,
-  `ip` tinytext,
-  `command` tinytext,
-  `command_time` tinytext,
-  `local_time` datetime default NULL,
-  `success` char(1) default NULL,
-  `ui` bigint(20) NOT NULL auto_increment,
+CREATE TABLE IF NOT EXISTS ftp_logs (
+  username tinytext,
+  filename text,
+  size bigint(20) default NULL,
+  host tinytext,
+  ip tinytext,
+  command tinytext,
+  command_time tinytext,
+  local_time datetime default NULL,
+  success char(1) default NULL,
+  ui bigint(20) NOT NULL auto_increment,
   PRIMARY KEY  (`ui`)
 ) TYPE=MyISAM;
