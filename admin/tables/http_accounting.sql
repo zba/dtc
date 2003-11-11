8ba713265f4d629e6e10367694517505c5d88c93
sirhexalo <sirhexalo>
2003-11-11 14:54:53 +0000

    WITH DOMAIN FIELD ...

--- admin/tables/http_accounting.sql
+++ admin/tables/http_accounting.sql
@@ -1,19 +1,22 @@
 #
 # Table structure for table `http_accounting`
 #
-
-CREATE TABLE IF NOT EXISTS `http_accounting` (
-  `id` int(14) NOT NULL auto_increment,
-  `vhost` varchar(50) NOT NULL default '',
-  `bytes_sent` int(14) NOT NULL default '0',
-  `count_hosts` int(12) NOT NULL default '0',
-  `count_visits` int(12) NOT NULL default '0',
-  `count_status_200` int(12) NOT NULL default '0',
-  `count_status_404` int(12) NOT NULL default '0',
-  `count_impressions` int(18) NOT NULL default '0',
-  `last_run` int(14) NOT NULL default '0',
-  `month` int(4) NOT NULL default '0',
-  `year` int(4) NOT NULL default '0',
-  PRIMARY KEY  (`id`)
+IF NOT EXITS
+CREATE TABLE http_accounting (
+  id int(14) NOT NULL auto_increment,
+  vhost varchar(50) NOT NULL default '',
+  bytes_sent int(14) NOT NULL default '0',
+  count_hosts int(12) NOT NULL default '0',
+  count_visits int(12) NOT NULL default '0',
+  count_status_200 int(12) NOT NULL default '0',
+  count_status_404 int(12) NOT NULL default '0',
+  count_impressions int(18) NOT NULL default '0',
+  last_run int(14) NOT NULL default '0',
+  month int(4) NOT NULL default '0',
+  year int(4) NOT NULL default '0',
+  domain varchar(50) NOT NULL default '',
+  PRIMARY KEY  (id)
 ) TYPE=MyISAM;
+
+#
 
