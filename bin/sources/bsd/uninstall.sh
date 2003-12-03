8b9d5697cd671e8e013a11e50998339537c6690f
thomas <thomas>
2003-12-03 00:52:58 +0000

    Updated BSD uninstall (now does not asks for path of deamon files). Updated texts of templates and switched some string to passwords...

--- bin/sources/bsd/uninstall.sh
+++ bin/sources/bsd/uninstall.sh
@@ -5,39 +5,7 @@
 # under LGPL Licence
 
 echo "### DEAMON PATH CONFIGURATION ###"
-
-echo ""
-echo "Where is located you httpd.conf ?"
-echo -n "httpd.conf path [/usr/local/apache/httpd.conf]: "
-read PATH_HTTPD_CONF
-if [ $PATH_HTTPD_CONF == ""];
-then
-	PATH_HTTPD_CONF="/usr/local/apache/httpd.conf"
-fi
-
-echo ""
-echo "Where is located your named.conf ?"
-echo -n "named.conf path [/etc/named.conf]: "
-read PATH_NAMED_CONF
-if [ $PATH_NAMED_CONF == ""];
-then
-	PATH_NAMED_CONF="/etc/named.conf"
-fi
-
-echo ""
-echo "Where is located your proftpd.conf ?"
-echo -n "Qmail control path [/etc/proftpd.conf]: "
-read PATH_PROFTPD_CONF
-if [ $PATH_PROFTPD_CONF == ""];
-then
-	PATH_PROFTPD_CONF="/etc/proftpd.conf"
-fi
-
-echo ""
-echo "Where is located your qmail control directory ?"
-echo -n "Qmail control path [/var/qmail/control]: "
-read PATH_QMAIL_CTRL
-if [ $PATH_QMAIL_CTRL == ""];
-then
-	PATH_QMAIL_CTRL="/var/qmail/control"
-fi
+PATH_HTTPD_CONF="/usr/local/apache/etc/httpd.conf"
+PATH_NAMED_CONF="/etc/namedb/named.conf"
+PATH_PROFTPD_CONF="/etc/local/etc/proftpd.conf"
+PATH_QMAIL_CTRL="/var/qmail/control"
