0d114f63d9e50ad90967148d605a70f760a67e7f
thomas <thomas>
2003-12-04 12:27:35 +0000

    Updated configuration for paiement system.

--- shared/securepay/paiement.php
+++ shared/securepay/paiement.php
@@ -1,26 +1,10 @@
 <?php
 
-$conf_use_paypal = "yes";
-$paypal_account = 'paypal@gplhost.com';
-
-$conf_use_worldpay = "yes";
-$wp_instid = "60943";
-$wp_curency = "USD";
-$wp_testmode = "100";
-$wp_callback_pass = ""; // Leave to blank if you don't want callbackPW to be used
-$wp_servers_ip = "195.35.90.61|195.35.90.62";
-$wp_accId1 = "";
-$wp_callback_url = "/dtc/secpaycallback_worldpay.php";
-$wt_md5_secret = "ILoveMD5Seccrets";
-
+require("$dtcshared_path/securepay/paiement_config.php");
 require("$dtcshared_path/securepay/pay_functions.php");
 
-if($conf_use_worldpay == "yes"){
-	include("$dtcshared_path/securepay/worldpay.php");
-}
-if($conf_use_paypal == "yes"){
-	include("$dtcshared_path/securepay/paypal.php");
-}
+if($conf_use_worldpay == "yes")	include("$dtcshared_path/securepay/worldpay.php");
+if($conf_use_paypal == "yes")	include("$dtcshared_path/securepay/paypal.php");
 
 function paynowButton($pay_id,$amount){
 	global $conf_use_paypal;
@@ -28,13 +12,13 @@ function paynowButton($pay_id,$amount){
 	$out .= "<table width=\"100%\" height=\"1\">";
 	$out .= "<tr><td>Paiement system</td><td>Cost</td><td>Total</td><td>Instant account</td></tr>\n";
 	if($conf_use_paypal == "yes"){
-		$cost = $amount * 1.04;
+		$cost = $amount * 0.04;
 		$total = $cost + $amount;
 		$out .= "<tr><td>".paypalButton($pay_id,$amount,$button_text)."</td>";
 		$out .= "<td>\$$cost</td><td>\$$total</td><td>No</td></tr>\n";
 	}
 	if($conf_use_worldpay == "yes"){
-		$cost = $amount * 1.0475 * 1.17;
+		$cost = $amount * (0.0475 + 0.017);
 		$total = $cost + $amount;
 		$out .= "<tr><td>".worldPayButton($pay_id,$total,$button_text,$button_text)."</td>";
 		$out .= "<td>\$$cost</td><td>\$$total</td><td>No</td></tr>\n";
