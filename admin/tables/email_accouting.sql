4b7decfe5f17e5b2e447d600de2a1090e68d252f
thomas <thomas>
2003-12-10 03:57:27 +0000

    The table creation for email accounting.

---
+++ admin/tables/email_accouting.sql
@@ -0,0 +1,10 @@
+CREATE TABLE IF NOT EXISTS email_accounting (
+  id int(11) NOT NULL auto_increment,
+  domain_name varchar(128) NOT NULL default '',
+  smtp_trafic int(14) unsigned NOT NULL default '0',
+  pop_trafic int(14) unsigned NOT NULL default '0',
+  month int(2) NOT NULL default '0',
+  year int(4) NOT NULL default '0',
+  UNIQUE KEY domain_name (domain_name,month,year),
+  UNIQUE KEY id (id)
+) TYPE=MyISAM;
---
+++ admin/tables/smtp_logs.sql
@@ -0,0 +1,18 @@
+CREATE TABLE  IF NOT EXISTS smtp_logs (
+  id int(11) NOT NULL auto_increment,
+  newmsg_id bigint(20) default NULL,
+  bounce_qp int(11) default NULL,
+  bytes int(11) NOT NULL default '0',
+  sender_user varchar(128) NOT NULL default '',
+  sender_domain varchar(128) NOT NULL default '',
+  delivery_id bigint(20) default NULL,
+  delivery_user varchar(128) NOT NULL default '',
+  delivery_domain varchar(128) NOT NULL default '',
+  delivery_success enum('yes','no') NOT NULL default 'no',
+  time_stamp timestamp(14) NOT NULL,
+  PRIMARY KEY  (id),
+  UNIQUE KEY bounce_qp (bounce_qp),
+  UNIQUE KEY newmsg_id (newmsg_id),
+  KEY sender_domain (sender_domain),
+  KEY delivery_domain (delivery_domain)
+) TYPE=MyISAM;
