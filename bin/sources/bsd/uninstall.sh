ebb038e9a8518e25319a564ed34f81f740dd589e
thomas <thomas>
2003-12-16 02:12:51 +0000

    Hop !

--- bin/sources/bsd/uninstall.sh
+++ bin/sources/bsd/uninstall.sh
@@ -7,5 +7,5 @@
 echo "### DEAMON PATH CONFIGURATION ###"
 PATH_HTTPD_CONF="/usr/local/apache/etc/httpd.conf"
 PATH_NAMED_CONF="/etc/namedb/named.conf"
-PATH_PROFTPD_CONF="/etc/local/etc/proftpd.conf"
+PATH_PROFTPD_CONF="/usr/local/etc/proftpd.conf"
 PATH_QMAIL_CTRL="/var/qmail/control"
