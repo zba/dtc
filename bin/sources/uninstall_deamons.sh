ca91a13dd2977c48d920466bb9f5b4737d7d01a3
thomas <thomas>
2003-12-16 03:49:41 +0000

    Corrected uninstall script for qmail files. Now uninstall is fully working.

--- bin/sources/uninstall_deamons.sh
+++ bin/sources/uninstall_deamons.sh
@@ -61,27 +61,22 @@ fi
 #
 
 echo "===> Uninstalling from qmail"
-#if ! [ -f $PATH_QMAIL_CTRL/locals.DTC.backup ] then
-#	rm -f $PATH_QMAIL_CTRL/locals
-#	mv -f $PATH_QMAIL_CTRL/locals.DTC.backup $PATH_QMAIL_CTRL/locals
-#fi
+if ! [ -f $PATH_QMAIL_CTRL/rcpthosts.DTC.backup ] ; then
+	rm -f $PATH_QMAIL_CTRL/rcpthosts
+	mv -f $PATH_QMAIL_CTRL/rcpthosts.DTC.backup $PATH_QMAIL_CTRL/rcpthosts
+fi
 
-#if ! [ -f $PATH_QMAIL_CTRL/rcpthosts.DTC.backup ] then
-#	rm -f $PATH_QMAIL_CTRL/rcpthosts
-#	mv -f $PATH_QMAIL_CTRL/rcpthosts.DTC.backup $PATH_QMAIL_CTRL/rcpthosts
-#fi
+if ! [ -f $PATH_QMAIL_CTRL/virtualdomains.DTC.backup ] ; then
+	rm -f $PATH_QMAIL_CTRL/virtualdomains.DTC.backup
+	mv -f $PATH_QMAIL_CTRL/virtualdomains.DTC.backup $PATH_QMAIL_CTRL/virtualdomains
+fi
 
-#if ! [ -f $PATH_QMAIL_CTRL/virtualdomains.DTC.backup ] then
-#	rm -f $PATH_QMAIL_CTRL/virtualdomains.DTC.backup
-#	mv -f $PATH_QMAIL_CTRL/virtualdomains.DTC.backup $PATH_QMAIL_CTRL/virtualdomains
-#fi
+if ! [ -f $PATH_QMAIL_CTRL/users/assign.DTC.backup ] ; then
+	rm -f $PATH_QMAIL_CTRL/users/assign.DTC.backup
+	mv -f $PATH_QMAIL_CTRL/users/assign.DTC.backup $PATH_QMAIL_CTRL/users/assign
+fi
 
-#if ! [ -f $PATH_QMAIL_CTRL/users/assign.DTC.backup ] then
-#	rm -f $PATH_QMAIL_CTRL/users/assign.DTC.backup
-#	mv -f $PATH_QMAIL_CTRL/users/assign.DTC.backup $PATH_QMAIL_CTRL/users/assign
-#fi
-
-#if ! [ -f /etc/poppasswd.DTC.backup ] then
-#	rm -f /etc/poppasswd.DTC.backup
-#	mv -f/etc/poppasswd.DTC.backup /etc/poppasswd
-#fi
+if ! [ -f /etc/poppasswd.DTC.backup ] ; then
+	rm -f /etc/poppasswd.DTC.backup
+	mv -f/etc/poppasswd.DTC.backup /etc/poppasswd
+fi
