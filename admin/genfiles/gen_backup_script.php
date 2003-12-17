27a4962c8dc832a5ebb6665f8c5d811bf90d74d8
thomas <thomas>
2003-12-17 01:00:51 +0000

    Added network based backup script.

--- admin/genfiles/gen_backup_script.php
+++ admin/genfiles/gen_backup_script.php
@@ -1,5 +1,65 @@
 <?php
 
+function backup_by_ftp(){
+	global $pro_mysql_domain_table;
+	global $pro_mysql_admin_table;
+	global $pro_mysql_subdomain_table;
+
+	global $conf_generated_file_path;
+	global $conf_backup_script_path;
+	global $conf_bakcup_path;
+
+	global $console;
+
+	$num_generated_vhosts=0;
+
+	$backup_net = "#!/bin/sh\n";
+
+	// Get the owner informations
+	$q = "SELECT adm_login,path FROM $pro_mysql_admin_table WHERE 1;";
+	$r = mysql_query ($q)or die("Cannot execute query \"$q\" !".mysql_error()." line ".__LINE__." file ".__FILE__);
+	$nr = mysql_num_rows($r);
+	for($i=0;$i<$nr;$i++){
+		$ra = mysql_fetch_array($r);
+		$owner = $ra["adm_login"];
+		$path = $ra["path"];
+		$backup_net .= "echo -n \"===> Backuping all files for user $owner:\"\n";
+		$backup_net .= "cd $path\n";
+		$q2 = "SELECT name FROM $pro_mysql_domain_table WHERE owner='$owner';";
+		$r2 = mysql_query ($q2)or die("Cannot execute query \"$q2\" !".mysql_error()." line ".__LINE__." file ".__FILE__);
+		$nr2 = mysql_num_rows($r2);
+		for($j=0;$j<$nr2;$j++){
+			$ra2 = mysql_fetch_array($r2);
+			$webname = $ra2["name"];
+			$backup_net .= "echo -n \"$webname (\"\n";
+			$backup_net .= "echo -n \"mail\"\n";
+			$backup_net .= "tar -czf $owner.$webname.tar.gz $webname/Mailboxs\n";
+			$q3 = "SELECT subdomain_name FROM $pro_mysql_subdomain_table WHERE domain_name='$webname';";
+			$r3 = mysql_query ($q3)or die("Cannot execute query \"$q3\" !".mysql_error()." line ".__LINE__." file ".__FILE__);
+			$nr3 = mysql_num_rows($r3);
+			for($k=0;$k<$nr3;$k++){
+				$ra3 = mysql_fetch_array($r3);
+				$subdom_name = $ra3["subdomain_name"];
+				$backup_net .= "echo -n \",$subdom_name\"\n";
+				$backup_net .= "tar -Azf $owner.$webname.tar.gz $webname/$subdom_name/html $webname/$subdom_name/cgi-bin\n";
+				$num_generated_vhosts++;
+			}
+			$backup_net .= "echo -n \")\"\n";
+			$backup_net .= "echo -n \"uploading\"\n";
+			$backup_net .= "ncftpput -f /root/.ncftpput_login.cfg /webserver/ftp/gplhost/ $owner.$webname.tar.gz\n";
+		}
+	}
+	$filep = fopen("$conf_generated_file_path/net_backup.sh", "w+");
+	if( $filep == NULL){
+		die("Cannot open file for writting");
+	}
+	fwrite($filep,$backup_net);
+	fclose($filep);
+	chmod("$conf_generated_file_path/$conf_backup_script_path",0750);
+	$console .= "Generated net-backup files for $num_generated_vhosts vhosts !<br>";
+
+}
+
 function backup_script_generate(){
 	global $pro_mysql_domain_table;
 	global $pro_mysql_admin_table;
@@ -12,6 +72,8 @@ function backup_script_generate(){
 	global $console;
 
 	$num_generated_vhosts=0;
+
+	backup_by_ftp();
 	
 	// Initialy delete last week backup
 	$backup_script = "#!/bin/bash
