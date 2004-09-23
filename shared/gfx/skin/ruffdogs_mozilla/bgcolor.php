9700c49d5e22926984b71baf4c2a95ae2ab2e274
thomas <thomas>
2004-09-23 17:29:26 +0000

    Updated skin.

--- shared/gfx/skin/ruffdogs_mozilla/bgcolor.php
+++ shared/gfx/skin/ruffdogs_mozilla/bgcolor.php
@@ -1,5 +1,5 @@
 <?php
-	$body_tag = "<link rel=\"stylesheet\" href=\"gfx/dtc.css\" type=\"text/css\">
+	$body_tag = "<link rel=\"stylesheet\" href=\"gfx/skin/ruffdogs_mozilla/ruffdogs.css\" type=\"text/css\">
 <body bgcolor=\"#C7D0D9\" text=\"#000000\" leftmargin=\"0\"
 topmargin=\"0\" marginwidth=\"0\" marginheight=\"0\">";
 
