7984f941e7ca41c185884d273864ce7882d203c7
thomas <thomas>
2004-10-05 17:41:49 +0000

    Forgot this one...

---
+++ shared/404_template/404.php
@@ -0,0 +1,25 @@
+<html>
+<body>
+<center>
+<table width="90%" height="100%">
+<tr><td width="100%" height="100%">
+<center><h1><u>404 Error: requested document does not exists!</u></h1>
+
+<font face="Arial">You have reach this page because the requested
+document does not exists on this server. Please check that the URL
+is correct or inform the webmaster of the website of an incorrect
+link.</font><br><br><br></td></tr>
+
+<tr><td width="100%" height="1"><center><font face="Arial" size="-2">
+This website server is powered by Domain Technologie Control (DTC),
+an open-source control panel<br>
+<i>Most of code done by:
+<a target="_blank" href="mailto:thomas@goirand.fr">Thomas GOIRAND</a>, under
+<a target="_blank" href="http://www.gnu.org">LGPL</a>. Please visit <a
+target="_blank" href="http://gplhost.com">GPLHost</a> and <a
+target="_blank" href="http://gplhost.com/?rub=softwares&sousrub=dtc">DTC
+home</a> for more infos.</i></font>
+</center></td></tr></table>
+</center>
+</body>
+</html>
