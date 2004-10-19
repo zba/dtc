ea2e15920e546d5fce36ddf4b10f7c7e2746ef90
thomas <thomas>
2004-10-19 17:14:52 +0000

    Added input checking function for new customers.

--- client/new_account_form.php
+++ client/new_account_form.php
@@ -1,5 +1,149 @@
 <?php
 
+function register_user(){
+// adm_login=aaa&reqadm_pass=&reqadm_pass2=&domain_name=toto&familyname=nymous&
+// firstname=ano&compname=testsoc&email=toto@toto.com&phone=PARIS&
+// fax=state&address1=1&address2=2&address3=3&zipcode=75991&city=paris&
+// state=state&country=US&Login=Register
+	if(($_REQUEST["reqadm_login"] == "" || !isset($_REQUEST["reqadm_login"]))
+	&& ($_REQUEST["reqadm_pass"] == "" || !isset($_REQUEST["reqadm_pass"]))
+	&& ($_REQUEST["reqadm_pass2"] == "" || !isset($_REQUEST["reqadm_pass2"]))
+	&& ($_REQUEST["domain_name"] == "" || !isset($_REQUEST["domain_name"]))
+	&& ($_REQUEST["familyname"] == "" || !isset($_REQUEST["familyname"]))
+	&& ($_REQUEST["firstname"] == "" || !isset($_REQUEST["firstname"]))
+	&& ($_REQUEST["email"] == "" || !isset($_REQUEST["email"]))
+	&& ($_REQUEST["phone"] == "" || !isset($_REQUEST["phone"]))
+	&& ($_REQUEST["address1"] == "" || !isset($_REQUEST["address1"]))
+	&& ($_REQUEST["zipcode"] == "" || !isset($_REQUEST["zipcode"]))
+	&& ($_REQUEST["city"] == "" || !isset($_REQUEST["city"]))
+	&& ($_REQUEST["firstname"] == "" || !isset($_REQUEST["firstname"]))){
+		$ret["err"] = 1;
+		$ret["mesg"] = "Not registering";
+	}
+	if(!isFtpLogin($_REQUEST["adm_login"])){
+		$ret["err"] = 2;
+		$ret["mesg"] = "User login format incorrect. Please use letters and numbers only and from 4 to 16 chars.";
+	}
+	if(!isDTCPassword($_REQUEST["reqadm_pass"])){
+		$ret["err"] = 2;
+		$ret["mesg"] = "Password format incorrect. Please use letters and numbers only and from 4 to 16 chars.";
+	}
+	if($_REQUEST["reqadm_pass"] != $_REQUEST["reqadm_pass2"]){
+		$ret["err"] = 2;
+		$ret["mesg"] = "Passwords 1 and 2 does not match!";
+	}
+	if(!isHostnameOrIP($_REQUEST["domain_name"])){
+		$ret["err"] = 2;
+		$ret["mesg"] = "Domain name does not look like to be a correct hostname.";
+	}
+	if(!isValidEmail($_REQUEST["email"])){
+		$ret["err"] = 2;
+		$ret["mesg"] = "Email does not look like to be a correct address format.";
+	}
+
+//	if($_REQUEST["
+
+	if(!isset($_REQUEST["familyname"]) || $_REQUEST["familyname"]==""){
+		$ret["err"] = 2;
+		$ret["mesg"] = "Required field family name missing.";
+	}else{
+		if (!get_magic_quotes_gpc()){
+			$esc_familyname = addslashes($_REQUEST["familyname"]);
+		}else{
+			$esc_familyname = $_REQUEST["familyname"];
+		}
+	}
+
+	if(!isset($_REQUEST["firstname"]) || $_REQUEST["firstname"]==""){
+		$ret["err"] = 2;
+		$ret["mesg"] = "Required field first name missing.";
+	}else{
+		if (!get_magic_quotes_gpc()){
+			$esc_firstname = addslashes($_REQUEST["firstname"]);
+		}else{
+			$esc_firstname = $_REQUEST["firstname"];
+		}
+	}
+
+	if(!isset($_REQUEST["phone"]) || $_REQUEST["phone"]==""){
+		$ret["err"] = 2;
+		$ret["mesg"] = "Required field phone missing.";
+	}else{
+		if (!get_magic_quotes_gpc()){
+			$esc_phone = addslashes($_REQUEST["phone"]);
+		}else{
+			$esc_phone = $_REQUEST["phone"];
+		}
+	}
+
+	if (!get_magic_quotes_gpc()){
+		$esc_compname = addslashes($_REQUEST["compname"]);
+	}else{
+		$esc_compname = $_REQUEST["compname"];
+	}
+
+	if (!get_magic_quotes_gpc()){
+		$esc_fax = addslashes($_REQUEST["fax"]);
+	}else{
+		$esc_fax = $_REQUEST["fax"];
+	}
+
+	if(!isset($_REQUEST["address1"]) || $_REQUEST["address1"]==""){
+		$ret["err"] = 2;
+		$ret["mesg"] = "Required field address missing.";
+	}else{
+		if (!get_magic_quotes_gpc()){
+			$esc_address1 = addslashes($_REQUEST["address1"]);
+		}else{
+			$esc_address1 = $_REQUEST["address1"];
+		}
+	}
+
+	if (!get_magic_quotes_gpc()){
+		$esc_address2 = addslashes($_REQUEST["address2"]);
+	}else{
+		$esc_address2 = $_REQUEST["address2"];
+	}
+
+	if (!get_magic_quotes_gpc()){
+		$esc_address3 = addslashes($_REQUEST["address3"]);
+	}else{
+		$esc_address3 = $_REQUEST["address3"];
+	}
+
+	if (!get_magic_quotes_gpc()){
+		$esc_zipcode = addslashes($_REQUEST["zipcode"]);
+	}else{
+		$esc_zipcode = $_REQUEST["zipcode"];
+	}
+
+	if (!get_magic_quotes_gpc()){
+		$esc_city = addslashes($_REQUEST["city"]);
+	}else{
+		$esc_city = $_REQUEST["city"];
+	}
+
+	if (!get_magic_quotes_gpc()){
+		$esc_state = addslashes($_REQUEST["state"]);
+	}else{
+		$esc_state = $_REQUEST["city"];
+	}
+
+	if(!ereg("^([A-Z])([A-Z])\$",$_REQUEST["country"])){
+		$ret["err"] = 2;
+		$ret["mesg"] = "Country code seems incorrect.";
+	}
+
+	if($_REQUEST["iscomp"] == "yes"){
+		$esc_comp = "yes";
+	}else if($_REQUEST["iscomp"] == "no"){
+		$esc_comp = "no";
+	}else{
+		$ret["err"] = 2;
+		$ret["mesg"] = "Is company radio button is wrong!";
+	}
+}
+
 function registration_form(){
 	global $txt_login_login;
 	global $txt_login_pass;
@@ -24,72 +168,77 @@ function registration_form(){
 	$login_info = "<table>
 <tr>
 	<td align=\"right\">".$txt_login_login[$lang]."</td>
-	<td><input type=\"text\" name=\"adm_login\" value=\"\"></td>
+	<td><input type=\"text\" name=\"reqadm_login\" value=\"".$_REQUEST["reqadm_login"]."\"></td>
 </tr><tr>
 	<td align=\"right\">".$txt_login_pass[$lang]."</td>
-	<td><input type=\"password\" name=\"adm_pass\" value=\"\"></td>
+	<td><input type=\"password\" name=\"reqadm_pass\" value=\"\"></td>
 </tr><tr>
 	<td align=\"right\">Confirm pass:</td>
-	<td><input type=\"password\" name=\"adm_pass2\" value=\"\"></td>
+	<td><input type=\"password\" name=\"reqadm_pass2\" value=\"\"></td>
 </tr><tr>
 	<td align=\"right\">Desired domain name:</td>
-	<td><input type=\"text\" name=\"adm_pass\" value=\"\"></td>
+	<td><input type=\"text\" name=\"domain_name\" value=\"".$_REQUEST["domain_name"]."\"></td>
 </tr></table>";
 	$login_skined = skin("frame",$login_info,"");
 
+	if($_REQUEST["iscomp"] == "yes"){
+		$compyes = "checked";
+	}else if($_REQUEST["iscomp"] == "no"){
+		$compno = " checked";
+	}
 	$client_info = "<table>
 <tr>
 	<td align=\"right\">".$txt_draw_client_info_familyname[$lang]."</td>
-	<td><input type=\"text\" name=\"familyname\" value=\"\"></td>
+	<td><input type=\"text\" name=\"familyname\" value=\"".$_REQUEST["familyname"]."\"></td>
 </tr><tr>
 	<td align=\"right\">".$txt_draw_client_info_firstname[$lang]."</td>
-	<td><input type=\"text\" name=\"firstname\" value=\"\"></td>
+	<td><input type=\"text\" name=\"firstname\" value=\"".$_REQUEST["firstname"]."\"></td>
 </tr><tr>
 	<td align=\"right\">Is company</td>
-	<td><input type=\"radio\" name=\"iscomp\" value=\"yes\">Yes
-<input type=\"radio\" name=\"iscomp\" value=\"no\">No</td>
+	<td><input type=\"radio\" name=\"iscomp\" value=\"yes\"$compyes>Yes
+<input type=\"radio\" name=\"iscomp\" value=\"no\"$compno>No</td>
 </tr><tr>
 	<td align=\"right\">".$txt_draw_client_info_comp_name[$lang]."</td>
-	<td><input type=\"text\" name=\"compname\" value=\"\"></td>
+	<td><input type=\"text\" name=\"compname\" value=\"".$_REQUEST["compname"]."\"></td>
 </tr><tr>
 	<td align=\"right\">".$txt_draw_client_info_email[$lang]."</td>
-	<td><input type=\"text\" name=\"email\" value=\"\"></td>
+	<td><input type=\"text\" name=\"email\" value=\"".$_REQUEST["email"]."\"></td>
 </tr><tr>
 	<td align=\"right\">".$txt_draw_client_info_phone[$lang]."</td>
-	<td><input type=\"text\" name=\"phone\" value=\"\"></td>
+	<td><input type=\"text\" name=\"phone\" value=\"".$_REQUEST["phone"]."\"></td>
 </tr><tr>
 	<td align=\"right\">".$txt_draw_client_info_fax[$lang]."</td>
-	<td><input type=\"text\" name=\"fax\" value=\"\"></td>
+	<td><input type=\"text\" name=\"fax\" value=\"".$_REQUEST["fax"]."\"></td>
 </tr></table>";
 	$client_skined = skin("frame",$client_info,"");
 
 	$client_addr = "<table>
 <tr>
 	<td align=\"right\">".$txt_draw_client_info_addr[$lang]."</td>
-	<td><input type=\"text\" name=\"addresse1\" value=\"\"></td>
+	<td><input type=\"text\" name=\"address1\" value=\"".$_REQUEST["address1"]."\"></td>
 </tr><tr>
 	<td align=\"right\">".$txt_draw_client_info_addr[$lang]." 2</td>
-	<td><input type=\"text\" name=\"addresse2\" value=\"\"></td>
+	<td><input type=\"text\" name=\"address2\" value=\"".$_REQUEST["address2"]."\"></td>
 </tr><tr>
 	<td align=\"right\">".$txt_draw_client_info_addr[$lang]." 3</td>
-	<td><input type=\"text\" name=\"addresse3\" value=\"\"></td>
+	<td><input type=\"text\" name=\"address3\" value=\"".$_REQUEST["address3"]."\"></td>
 </tr><tr>
 	<td align=\"right\">".$txt_draw_client_info_zipcode[$lang]."</td>
-	<td><input type=\"text\" name=\"zipcode\" value=\"\"></td>
+	<td><input type=\"text\" name=\"zipcode\" value=\"".$_REQUEST["zipcode"]."\"></td>
 </tr><tr>
 	<td align=\"right\">".$txt_draw_client_info_city[$lang]."</td>
-	<td><input type=\"text\" name=\"phone\" value=\"\"></td>
+	<td><input type=\"text\" name=\"city\" value=\"".$_REQUEST["city"]."\"></td>
 </tr><tr>
 	<td align=\"right\">".$txt_draw_client_info_state[$lang]."</td>
-	<td><input type=\"text\" name=\"fax\" value=\"\"></td>
+	<td><input type=\"text\" name=\"state\" value=\"".$_REQUEST["state"]."\"></td>
 </tr><tr>
 	<td align=\"right\">".$txt_draw_client_info_country[$lang]."</td>
-	<td><select name=\"country\">".cc_code_popup()."</select></td>
+	<td><select name=\"country\">".cc_code_popup($_REQUEST["country"])."</select></td>
 </tr></table>";
 	$addr_skined = skin("frame",$client_addr,"");
 
 	$HTML_admin_edit_data = "<a href=\"/dtc\">Go to login</a>
-<form action=\"".$_SERVER["PHP_SELF"]."\" method=\"post\">
+<form action=\"".$_SERVER["PHP_SELF"]."\" methode=\"post\">
 <table>
 <tr>
 	<td>Login info:$login_skined</td>
