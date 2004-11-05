9c96718639c43b76e2debf06a81c7434626d8c2c
thomas <thomas>
2004-11-05 18:00:06 +0000

    HOP!

--- shared/securepay/paiement.php
+++ shared/securepay/paiement.php
@@ -15,7 +15,7 @@ function paynowButton($pay_id,$amount,$item_name,$return_url){
 		$total = $amount * 1.034 + 0.30;
 		$cost = $total - $amount;
 		$out .= "<tr><td>".paypalButton($pay_id,$total,$item_name,$return_url)."</td>";
-		$out .= "<td>$amount</td><td>\$$cost</td><td>\$$total</td><td>No</td></tr>\n";
+		$out .= "<td>\$$amount</td><td>\$$cost</td><td>\$$total</td><td>No</td></tr>\n";
 	}
 	if($conf_use_worldpay == "yes"){
 		$total = $amount * 1.0475+ 0.017;
