906e1459890cafaf8764dc6830ee0e267c71e33b
thomas <thomas>
2004-11-15 16:06:07 +0000

    Updated default 404 page.

--- shared/404_template/404.php
+++ shared/404_template/404.php
@@ -5,12 +5,13 @@
 <table width="90%" height="100%">
 <tr><td width="100%" height="100%">
 <center><h1><u>404 Error: requested document does not exists!</u></h1>
-<font face="Verdana" size="-2"><?php echo $_SERVER["SCRIPT_URI"];
+<font face="Verdana" size="-2"><?php echo "URL: http://".$_SERVER["SERVER_NAME"].$_SERVER["REQUEST_URI"];
 ?></font><br><br>
 <font face="Arial">You have reach this page because the requested
 document does not exists on this server. Please check that the URL
 is correct or inform the webmaster of the website of an incorrect
-link.</font><br><br><br></td></tr>
+link.<br><br><a href="<?php echo "http://".$_SERVER["SERVER_NAME"]?>">Go to
+website home page</a></font><br><br><br></td></tr>
 
 <tr><td width="100%" height="1"><center><font face="Arial" size="-2">
 This website server is powered by Domain Technologie Control (DTC),
