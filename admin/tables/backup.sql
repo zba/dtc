c21fb9be2af936ba953b741fcbe5e77494cc64ac
thomas <thomas>
2004-12-19 16:30:45 +0000

    Added tables for backup MX/DNS and secpayconfig.

---
+++ admin/tables/backup.sql
@@ -0,0 +1,9 @@
+CREATE TABLE IF NOT EXISTS backup (
+  id int(9) NOT NULL auto_increment,
+  server_addr varchar(128) NOT NULL default '',
+  server_login varchar(128) NOT NULL default '',
+  server_pass varchar(128) NOT NULL default '',
+  type enum('grant_access','mail_backup','dns_backup','backup_ftp_to') NOT NULL default 'grant_access',
+  PRIMARY KEY  (id),
+  UNIQUE KEY id (id)
+) TYPE=MyISAM
---
+++ admin/tables/secpayconf.sql
@@ -0,0 +1,9 @@
+CREATE TABLE IF NOT EXISTS secpayconf (
+  unicrow int(2) NOT NULL default '0',
+  use_paypal enum('yes','no') NOT NULL default 'no',
+  paypal_rate float(6,2) NOT NULL default '0.00',
+  paypal_flat float(6,2) NOT NULL default '0.00',
+  paypal_autovalidate enum('yes','no') NOT NULL default 'yes',
+  paypal_email varchar(128) NOT NULL default 'palpay@gplhost.com',
+  UNIQUE KEY unicrow (unicrow)
+) TYPE=MyISAM
