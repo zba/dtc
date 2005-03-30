d094718e504fc1256d7200103c1ab28563092fc4
thomas <thomas>
2005-03-30 12:43:22 +0000

    Added packager.php for automatic package installation.

---
+++ shared/inc/forms/packager.php
@@ -0,0 +1,19 @@
+<?php
+
+////////////////////////////////////////////////////
+// One domain name ftp account collection edition //
+////////////////////////////////////////////////////
+function drawAdminTools_PackageInstaller($domain,$adm_path){
+	global $adm_login;
+	global $adm_pass;
+	global $edit_domain;
+	global $addrlink;
+
+	global $lang;
+
+	$txt = "<h3>Choose a package to install:</h3>";
+
+	return $txt;
+}
+
+?>
