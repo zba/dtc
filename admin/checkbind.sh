5d189b1eb3473ee974e7dc2e1de625e2d2e2d4ad
tusker <tusker>
2005-04-13 12:20:50 +0000

    Change to bind:nogroup rather than nobody:bind

--- admin/checkbind.sh
+++ admin/checkbind.sh
@@ -8,9 +8,9 @@ fi
 
 if [ ""$bindgroup != "" ]; then
 	echo "Changing "$bindgroup $conf_generated_file_path"/zones permissions to nobody:"$bindgroup
-        chown -R nobody:""$bindgroup $conf_generated_file_path/zones
+        chown -R $bindgroup:""65534 $conf_generated_file_path/zones
 	chmod -R 0770 $conf_generated_file_path/zones
-        chown -R nobody:""$bindgroup $conf_generated_file_path/slave_zones
+        chown -R $bindgroup:""65534 $conf_generated_file_path/slave_zones
 	chmod -R 0770 $conf_generated_file_path/slave_zones
 else
 	echo "Didn't find any groups for named, it must be running as root"
