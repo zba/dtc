96f108d4fbeb0ffd08008d9b4479c15ffe7a79df
thomas <thomas>
2005-05-05 23:22:06 +0000

    Added a new skin by Tex.

---
+++ shared/gfx/skin/tex/bgcolor.php.bak
@@ -0,0 +1,5 @@
+<?php
+	$body_tag = "<link rel=\"stylesheet\" href=\"gfx/skin/muedgrey/amp_style.css\" type=\"text/css\">
+<body background=\"gfx/skin/muedgrey/degrade.jpg\" bgcolor=\"#EEEEEE\" text=\"#000000\" leftmargin=\"0\" topmargin=\"0\" marginwidth=\"0\" marginheight=\"0\">";
+
+?>
---
+++ shared/gfx/skin/tex/skin.php
@@ -0,0 +1,20 @@
+<?php
+
+$skinTable[] = array(
+	"skinName" => 'tex',
+	"skinCss" => 'skin.css',
+	"functionCode" => '
+	$skinpath = "$skinGeneralPath/$skinpath";
+
+	return "<table cellpadding=\"1\" width=\"100%\"><tr><td><div class=TEXwincontainer>
+<div class=TEXwintitle>
+$title
+</div>
+<div class=TEXwinbody>
+$content
+</div>
+</div></td></tr></table>";
+'
+	);
+
+?>
