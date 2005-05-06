b64bea5c3072c246de8fa00d3b94c14db2e11154
thomas <thomas>
2005-05-06 00:42:02 +0000

    Wow ! A lot better with white bg ! :)

--- shared/gfx/skin/tex/bgcolor.php
+++ shared/gfx/skin/tex/bgcolor.php
@@ -1,5 +1,5 @@
 <?php
 	$body_tag = "<link rel=\"stylesheet\" href=\"gfx/skin/muedgrey/amp_style.css\" type=\"text/css\">
-<body bgcolor=\"#EEEEEE\" text=\"#000000\" leftmargin=\"0\" topmargin=\"0\" marginwidth=\"0\" marginheight=\"0\">";
+<body bgcolor=\"#FFFFFF\" text=\"#000000\" leftmargin=\"0\" topmargin=\"0\" marginwidth=\"0\" marginheight=\"0\">";
 
 ?>
