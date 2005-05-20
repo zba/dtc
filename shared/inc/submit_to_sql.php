8c48890fa55fd5f3e758794dc1df25f4d75b0c4a
thomas <thomas>
2005-05-20 10:39:31 +0000

    Spelling error...

--- shared/inc/submit_to_sql.php
+++ shared/inc/submit_to_sql.php
@@ -223,7 +223,7 @@ if($panel_type!="email"){
 	require("$dtcshared_path/inc/sql/subdomain.php");
 	require("$dtcshared_path/inc/sql/ftp.php");
 	require("$dtcshared_path/inc/sql/email.php");
-	require("$dtcshared_path/inc/sql/resseller.php");
+	require("$dtcshared_path/inc/sql/reseller.php");
 }else{
 	require("submit_to_sql_dtcemail.php");
 }
