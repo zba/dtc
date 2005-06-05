1d0c68eb450bcf2d405d48e066b5a13afe4a57b3
thomas <thomas>
2005-06-05 18:56:26 +0000

    Hop!

--- shared/inc/forms/packager.php
+++ shared/inc/forms/packager.php
@@ -11,15 +11,23 @@ function drawAdminTools_PackageInstaller($domain,$adm_path){
 	global $dtcshared_path;
 	global $lang;
 
-	$txt = "<h3>Choose a package to install:</h3>";
+	$txt = "<b><u>Choose a package to install:</u></b>";
 
-	$txt .= "<table cellspacing=\"0\" cellpadding=\"0\" border=\"0\">";
+	$txt .= "<table cellspacing=\"0\" cellpadding=\"4\" border=\"1\">";
+	$txt .= "<tr><td>Name</td><td>Description</td><td>Version</td><td>Need a database</td><td>Install</td></tr>";
 	$dir = $dtcshared_path."/package-installer";
 	if (is_dir($dir)) {
 		if ($dh = opendir($dir)) {
 			while (($file = readdir($dh)) !== false) {
 				if(is_dir($dir."/".$file) && $file != "." && $file != ".."){
-					$txt .= "<td>".$file."</td></tr>";
+					if(file_exists($dir."/".$file."/dtc-pkg-info.php")){
+						include($dir."/".$file."/dtc-pkg-info.php");
+						$txt .= "<tr><td>".$pkg_info["name"]."</td>
+							<td>".$pkg_info["short_desc"]."</td>
+							<td>".$pkg_info["version"]."</td>
+							<td>".$pkg_info["need_database"]."</td>
+							<td><a href=\"".$_SERVER["PHP_SELF"]."?adm_login=$adm_login&adm_pass=$adm_pass&addrlink=$addrlink\">INSTALL</a></td></tr>";
+					}
 				}
 			}
 			closedir($dh);
