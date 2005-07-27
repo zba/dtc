6928224b9c523c8ec8b07336b6dbbc72f517f1e4
thomas <thomas>
2005-07-27 17:57:20 +0000

    Added phpBB2 installer.

--- shared/drawlib/dtc_functions.php
+++ shared/drawlib/dtc_functions.php
@@ -1,5 +1,43 @@
 <?php
 
+function HTTP_Post($URL,$data, $referrer=""){
+	$result = "";
+	// parsing the given URL
+	$URL_Info=parse_url($URL);
+
+	// Building referrer
+	if($referrer=="") // if not given use this script as referrer
+		$referrer=$_SERVER["SCRIPT_URI"];
+
+	// making string from $data
+	foreach($data as $key=>$value)
+		$values[]="$key=".urlencode($value);
+	$data_string=implode("&",$values);
+
+	// Find out which port is needed - if not given use standard (=80)
+	if(!isset($URL_Info["port"]))
+		$URL_Info["port"]=80;
+
+	// building POST-request:
+	$request = "";
+	$request.="POST ".$URL_Info["path"]." HTTP/1.1\n";
+	$request.="Host: ".$URL_Info["host"]."\n";
+	$request.="Referer: $referrer\n";
+	$request.="Content-type: application/x-www-form-urlencoded\n";
+	$request.="Content-length: ".strlen($data_string)."\n";
+	$request.="Connection: close\n";
+	$request.="\n";
+	$request.=$data_string."\n";
+
+	$fp = fsockopen($URL_Info["host"],$URL_Info["port"]);
+	fputs($fp, $request);
+	while(!feof($fp)){
+		$result .= fgets($fp, 128);
+	}
+	fclose($fp);
+	return $result;
+}
+
 function logPay($txt){
 	$fp = fopen("/tmp/paylog.txt","a");
 	fwrite($fp,$txt."\n");
--- shared/inc/forms/packager.php
+++ shared/inc/forms/packager.php
@@ -15,6 +15,11 @@ function drawAdminTools_PackageInstaller($domain,$adm_path){
 
 	global $conf_mysql_db;
 
+	global $package_installer_console;
+	global $dtcpkg_db_login;
+
+	global $pkg_info;
+
 	$txt = "";
 	$dir = $dtcshared_path."/package-installer";
 
@@ -27,6 +32,7 @@ function drawAdminTools_PackageInstaller($domain,$adm_path){
 		include($dtc_pkg_info);
 	}
 	if(isset($_REQUEST["action"]) && $_REQUEST["action"] == "do_install"){
+		// Check if user has enough rights
 		checkLoginPassAndDomain($adm_login,$adm_pass,$edit_domain);
 		checkSubdomainFormat($_REQUEST["subdomain"]);
 		$admin_path = getAdminPath($adm_login);
@@ -34,27 +40,74 @@ function drawAdminTools_PackageInstaller($domain,$adm_path){
 		if(!is_dir($target)){
 			die("Destination directory does not exists line ".__LINE__." file ".__FILE__);
 		}
+
+		// Unpack the distribution package in target folder
 		if($pkg_info["unpack_type"] == "tar.gz"){
 			$cmd = "tar -C $target -xvzf $pkg_path/".$pkg_info["file"];
-			$x = "tar -xvzf ".$pkg_info["file"]."\n";
+			$x = "=> tar -xvzf ".$pkg_info["file"]."\n";
 			exec($cmd,$exec_out,$return_val);
 		}else if($pkg_info["unpack_type"] == "tar.bz2"){
 			$cmd = "tar -C $target -xvjf $pkg_path/".$pkg_info["file"];
-			$x = "tar -xvjf ".$pkg_info["file"]."\n";
+			$x = "=> tar -xvjf ".$pkg_info["file"]."\n";
 			exec($cmd,$exec_out,$return_val);
 		}else{
 			die("Package methode not supported yet");
 		}
+
+		// Rename folder to the destination folder name (eg remove version out of package.X.X.X folder name if exists)
 		if(isset($pkg_info["renamedir_to"]) && isset($pkg_info["resulting_dir"]) && $pkg_info["resulting_dir"] != $pkg_info["renamedir_to"]){
 			$cmd = "mv $target/".$pkg_info["resulting_dir"]." $target/".$pkg_info["renamedir_to"];
-			$x .= "mv".$pkg_info["resulting_dir"]." ".$pkg_info["renamedir_to"]."\n";
+			$x .= "=> Moving to folder ".$pkg_info["renamedir_to"]."<br>";
 			exec($cmd,$exec_out,$return_val);
 		}
-		$txt .= "<b><u>Installation of ".$pkg_info["name"].":</u></b><br><pre>$x</pre>";
+// https://dtc.gpl-host.com/dtc/index.php?adm_login=zigo&adm_pass=toto&addrlink=gpl-host.com/package-installer&action=prepareinstall&pkg=phpbb
+		// Move the folder to the requested name dtcpkg_directory=bla
+		if($_REQUEST["dtcpkg_directory"] == ""){
+			$cmd = "mv $target/".$pkg_info["renamedir_to"]."/* $target/";
+			$x .= "=> Moving to folder $target/<br>";
+			exec($cmd,$exec_out,$return_val);
+			$realtarget = "$target";
+		}else{
+			$cmd = "mv $target/".$pkg_info["renamedir_to"]." $target/".$_REQUEST["dtcpkg_directory"];
+			$x .= "=> Moving to folder $target/".$_REQUEST["dtcpkg_directory"]."<br>";
+			exec($cmd,$exec_out,$return_val);
+			$realtarget = "$target/".$_REQUEST["dtcpkg_directory"];
+		}
+
+		// Get the database infos beffore calling the custom package installer
+		$q = "SELECT db.Db,db.User FROM mysql.user,mysql.db WHERE user.dtcowner='$adm_login' AND db.User=user.User AND db.Db='".$_REQUEST["database_name"]."';";
+		$r = mysql_query($q)or die("Cannot query $q line ".__LINE__." file ".__FILE__." sql said: ".mysql_error());
+		$n = mysql_num_rows($r);
+		if($n != 1)die("Cannot find database line ".__LINE__." file ".__FILE__);
+		$a = mysql_fetch_array($r);
+		$dtcpkg_db_login = $a["User"];
+
+		// Call the package specific installer php script
+		$install_ret = do_package_install();
+		if($install_ret == 0){
+			$package_installer_console .= "Install successful !<br>";
+		}
+		$nbr_remove = sizeof($pkg_info["remove_folder_path"]);
+		if($nbr_remove > 0){
+			print_r($pkg_info["remove_folder_path"]);
+			$nbr_remove = sizeof($pkg_info["remove_folder_path"]);
+			$package_installer_console .= "Removing install folders...<br>";
+			for($i=0;$i<$nbr_remove;$i++){
+				$cmd = "rm -r $realtarget/".$pkg_info["remove_folder_path"][$i];
+				$package_installer_console .= $cmd."<br>";
+				exec($cmd,$exec_out,$return_val);
+			}
+		}
+
+		// Print the results
+		$txt .= "<b><u>Installation of ".$pkg_info["name"].":</u></b><br><pre>".$x.$package_installer_console."</pre>";
 		return $txt;
 	}
 
 	if(isset($_REQUEST["action"]) && $_REQUEST["action"] == "prepareinstall"){
+//		echo "<pre>";
+//		print_r($pkg_info);
+//		echo "</pre>";
 		$txt = "<b><u>You are about to install ".$pkg_info["name"].":</u></b><br>
 		<u>Description:</u> ".$pkg_info["long_desc"]."<br>
 		<u>Version:</u> ".$pkg_info["version"]."<br><br>";
@@ -66,7 +119,7 @@ function drawAdminTools_PackageInstaller($domain,$adm_path){
 		if($pkg_info["need_database"] == "yes"){
 			$txt .= "<b><u>Choose a database name for setup:</u></b><br>";
 			mysql_select_db("mysql")or die ("Cannot select db: mysql");
-			$q = "SELECT db.Db FROM user,db
+			$q = "SELECT db.Db,db.User FROM user,db
 			WHERE user.dtcowner='$adm_login'
 			AND db.User=user.User";
 			$r = mysql_query($q)or die("Cannot query $q line ".__LINE__." file ".__FILE__." sql said: ".mysql_error());
@@ -76,19 +129,35 @@ function drawAdminTools_PackageInstaller($domain,$adm_path){
 				(click database in the menu, then create a user and a database for this user).";
 				return $txt;
 			}
-			$txt .= "<select name=\"database_name\">";
+			$txt .= "Database name: <select name=\"database_name\">";
 			for($i=0;$i<$n;$i++){
 				$a = mysql_fetch_array($r);
-				$txt .= "<option value=\"".$a["Db"]."\">".$a["Db"]."</option>";
+				$txt .= "<option value=\"".$a["Db"]."\">".$a["Db"]." (login: ".$a["User"].")"."</option>";
 			}
-			$txt .= "</select><br><br>";
-			mysql_select_db($conf_mysql_db)or die ("Cannot select db: $conf_mysql_db");
+			$txt .= "</select><br>
+				Database password: <input type=\"password\" name=\"dtcpkg_db_pass\" value=\"\"><br><br>";
+			mysql_select_db($conf_mysql_db)or die ("Cannot select db: $conf_mysql_db line ".__LINE__." file ".__FILE__);
 		}
+
+		if($pkg_info["need_admin_email"] == "yes"){
+			$txt .= "<b><u>Enter email for the admin of this package:</u></b><br>";
+			$txt .= "Email: <input type=\"text\" name=\"dtcpkg_email\" value=\"\"><br><br>";
+		}
+
+		if($pkg_info["need_admin_login"] == "yes"){
+			$txt .= "<b><u>Enter login informations for the admin of this package:</u></b><br>";
+			$txt .= "Login: <input type=\"text\" name=\"dtcpkg_login\" value=\"\"><br>";
+			if($pkg_info["need_admin_pass"] == "yes"){
+				$txt .= "Pass: <input type=\"text\" name=\"dtcpkg_pass\" value=\"\"><br>";
+			}
+			$txt .= "<br>";
+		}
+
 
 		$txt .= "<b><u>Choose the subdomain and install :</u></b><br>";
 		$txt .= "<input type=\"hidden\" name=\"action\" value=\"do_install\">
 		<input type=\"hidden\" name=\"pkg\" value=\"".$_REQUEST["pkg"]."\">
-		<select name=\"subdomain\">";
+		Subdomain: <select name=\"subdomain\">";
 //		echo "<pre>";
 //		print_r($domain);
 //		echo "</pre>";
@@ -96,7 +165,15 @@ function drawAdminTools_PackageInstaller($domain,$adm_path){
 		for($i=0;$i<$n;$i++){
 			$txt .= "<option value=\"".$domain["subdomains"][$i]["name"]."\">".$domain["subdomains"][$i]["name"]."</option>";
 		}
-		$txt .= "</select><input type=\"submit\" value=\"Install\"></form>";
+		$txt .= "</select><br><br>";
+
+		if($pkg_info["can_select_directory"] == "yes"){
+			$txt .= "<b><u>Enter the directory where you want to install this package:</u></b><br>";
+			$txt .= "Directory (blank for /): <input type=\"text\" name=\"dtcpkg_directory\" value=\"\"><br><br>";
+		}
+
+		$txt .= "<input type=\"submit\" value=\"Install\">";
+		$txt .= "</form>";
 		return $txt;
 	}
 	$txt = "<b><u>Choose a package to install:</u></b>";
