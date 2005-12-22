da66251d342de34c34293b88cab6fae243b7929d
thomas <thomas>
2005-12-22 00:21:50 +0000

    Stats each hours is more than enough.

--- admin/cron.php
+++ admin/cron.php
@@ -213,9 +213,9 @@ function updateAllDomainsStats(){
 // This will set each day at 0:00
 // if(($start_stamps%(60*60*24))< 60*10)	updateAllDomainsStats();
 // This one is each hours
-// if(($start_stamps%(60*60))< 60*10)	updateAllDomainsStats();
+if(($start_stamps%(60*60))< 60*10)	updateAllDomainsStats();
 // This is each time the script is launched (all 10 minutes)
-updateAllDomainsStats();
+// updateAllDomainsStats();
 
 // Re-read cronjob values as long as they could have change
 // during this long job calculation !
