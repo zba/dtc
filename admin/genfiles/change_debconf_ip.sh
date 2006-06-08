48204a3db643bd0119df7312fa27b605566487d9
tusker <tusker>
2006-06-08 11:07:17 +0000

    Added usage info

--- admin/genfiles/change_debconf_ip.sh
+++ admin/genfiles/change_debconf_ip.sh
@@ -1,5 +1,6 @@
 #!/bin/sh -e
 
+
 # Source debconf library.
 . /usr/share/debconf/confmodule
 
@@ -10,6 +11,10 @@ db_get dtc/conf_ipaddr
 conf_ipaddr=$RET
 echo "IP was set as $conf_ipaddr"
 
+if [ $# -lt 1 ]; then
+echo "Usage: $0 <new IP>"
+fi
+exit 
 echo "We want the IP to be $1"
 
 db_set dtc/conf_ipaddr $1
