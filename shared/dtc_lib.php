9140fd8bc78593660e220c7ba22b374926a47cac
thomas <thomas>
2006-07-17 06:58:10 +0000

    A small syntax problem...

--- shared/dtc_lib.php
+++ shared/dtc_lib.php
@@ -31,7 +31,7 @@ require("$dtcshared_path/drawlib/skinLib.php");			// This is the new modular ski
 require("$dtcshared_path/inc/fetchmail.php");
 
 // The soap API to connect to xen servers
-if (!function_exists(soapclient)){
+if (!function_exists("soapclient")){
 	require("$dtcshared_path/inc/nusoap.php");
 }
 
