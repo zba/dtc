4274a9aad587cef544f4b41262d08afd0e32f07d
tusker <tusker>
2006-07-27 08:12:53 +0000

    Fix addition for LOG_SQL

--- bin/sources/configure_deamons.sh
+++ bin/sources/configure_deamons.sh
@@ -549,7 +549,7 @@ if [ -e ${APACHE2_CONFD} ] ; then
 	fi
 	TMP_FILE=`${MKTEMP} DTC_install_conf.d_apache2.XXXXXX` || exit 1
 	echo "# Configured by DTC $VERSION" >> $TMP_FILE
-	echo "APACHE2_OPTS=\"$APACHE2_OPTS -D LOG_SQL\"" >> $TMP_FILE
+	echo "APACHE2_OPTS=\"\$APACHE2_OPTS -D LOG_SQL\"" >> $TMP_FILE
 	echo "# End of DTC configuration $VERSION" >> $TMP_FILE
 
 	# now to insert it at the end of the actual $APACHE2_CONFD
