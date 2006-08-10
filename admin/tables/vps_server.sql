25ea0f97d3d5b44a16cef2e97f1d5eb0d8458d97
thomas <thomas>
2006-08-10 15:30:23 +0000

    Woooooooooooops a coma missing !

--- admin/tables/vps_server.sql
+++ admin/tables/vps_server.sql
@@ -3,7 +3,7 @@ CREATE TABLE IF NOT EXISTS vps_server (
   hostname varchar(255) NOT NULL default '',
   location varchar(64) NOT NULL default '',
   soap_login varchar(64) NOT NULL default '',
-  soap_pass varchar(64) NOT NULL default ''
+  soap_pass varchar(64) NOT NULL default '',
   PRIMARY KEY  (id),
   UNIQUE KEY hostname (hostname)
 ) TYPE=MyISAM;
