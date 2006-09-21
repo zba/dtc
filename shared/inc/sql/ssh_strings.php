d522abb58e7bae4788cdd5038db1e7bc025a7e6e
thomas <thomas>
2006-09-21 20:49:14 +0000

    Added ssh_strings.php

---
+++ shared/inc/sql/ssh_strings.php
@@ -0,0 +1,588 @@
+<?php
+
+$txt_sshsql_you_dont_have_the_ssh_login = array(
+	"fr" => "Vous n'avez pas les droits pour le SSH!",
+	"en" => "You don't have the SSH login flag!",
+	"hu" => "TRANS You don't have the SSH login flag!",
+	"it" => "TRANS You don't have the SSH login flag!",
+	"nl" => "TRANS You don't have the SSH login flag!",
+	"ru" => "TRANS You don't have the SSH login flag!",
+	"de" => "TRANS You don't have the SSH login flag!",
+	"zh" => "TRANS You don't have the SSH login flag!",
+	"pl" => "TRANS You don't have the SSH login flag!",
+	"se" => "TRANS You don't have the SSH login flag!",
+	"es" => "TRANS You don't have the SSH login flag!",
+	"pt" => "TRANS You don't have the SSH login flag!"
+);
+
+$txt_sshsql_your_path_is_restricted_to = array(
+	"fr" => "Votre chemin est restreint à ",
+	"en" => "Your path is restricted to ",
+	"hu" => "TRANS Your path is restricted to ",
+	"it" => "TRANS Your path is restricted to ",
+	"nl" => "TRANS Your path is restricted to ",
+	"ru" => "TRANS Your path is restricted to ",
+	"de" => "TRANS Your path is restricted to ",
+	"zh" => "TRANS Your path is restricted to ",
+	"pl" => "TRANS Your path is restricted to ",
+	"se" => "TRANS Your path is restricted to ",
+	"es" => "TRANS Your path is restricted to ",
+	"pt" => "TRANS Your path is restricted to "
+);
+
+$txt_sshsql_ = array(
+	"fr" => "",
+	"en" => "",
+	"hu" => "TRANS ",
+	"it" => "TRANS ",
+	"nl" => "TRANS ",
+	"ru" => "TRANS ",
+	"de" => "TRANS ",
+	"zh" => "TRANS ",
+	"pl" => "TRANS ",
+	"se" => "TRANS ",
+	"es" => "TRANS ",
+	"pt" => "TRANS "
+);
+
+$txt_sshsql_ = array(
+	"fr" => "",
+	"en" => "",
+	"hu" => "TRANS ",
+	"it" => "TRANS ",
+	"nl" => "TRANS ",
+	"ru" => "TRANS ",
+	"de" => "TRANS ",
+	"zh" => "TRANS ",
+	"pl" => "TRANS ",
+	"se" => "TRANS ",
+	"es" => "TRANS ",
+	"pt" => "TRANS "
+);
+
+$txt_sshsql_ = array(
+	"fr" => "",
+	"en" => "",
+	"hu" => "TRANS ",
+	"it" => "TRANS ",
+	"nl" => "TRANS ",
+	"ru" => "TRANS ",
+	"de" => "TRANS ",
+	"zh" => "TRANS ",
+	"pl" => "TRANS ",
+	"se" => "TRANS ",
+	"es" => "TRANS ",
+	"pt" => "TRANS "
+);
+
+$txt_sshsql_ = array(
+	"fr" => "",
+	"en" => "",
+	"hu" => "TRANS ",
+	"it" => "TRANS ",
+	"nl" => "TRANS ",
+	"ru" => "TRANS ",
+	"de" => "TRANS ",
+	"zh" => "TRANS ",
+	"pl" => "TRANS ",
+	"se" => "TRANS ",
+	"es" => "TRANS ",
+	"pt" => "TRANS "
+);
+
+$txt_sshsql_ = array(
+	"fr" => "",
+	"en" => "",
+	"hu" => "TRANS ",
+	"it" => "TRANS ",
+	"nl" => "TRANS ",
+	"ru" => "TRANS ",
+	"de" => "TRANS ",
+	"zh" => "TRANS ",
+	"pl" => "TRANS ",
+	"se" => "TRANS ",
+	"es" => "TRANS ",
+	"pt" => "TRANS "
+);
+
+$txt_sshsql_ = array(
+	"fr" => "",
+	"en" => "",
+	"hu" => "TRANS ",
+	"it" => "TRANS ",
+	"nl" => "TRANS ",
+	"ru" => "TRANS ",
+	"de" => "TRANS ",
+	"zh" => "TRANS ",
+	"pl" => "TRANS ",
+	"se" => "TRANS ",
+	"es" => "TRANS ",
+	"pt" => "TRANS "
+);
+
+$txt_sshsql_ = array(
+	"fr" => "",
+	"en" => "",
+	"hu" => "TRANS ",
+	"it" => "TRANS ",
+	"nl" => "TRANS ",
+	"ru" => "TRANS ",
+	"de" => "TRANS ",
+	"zh" => "TRANS ",
+	"pl" => "TRANS ",
+	"se" => "TRANS ",
+	"es" => "TRANS ",
+	"pt" => "TRANS "
+);
+
+$txt_sshsql_ = array(
+	"fr" => "",
+	"en" => "",
+	"hu" => "TRANS ",
+	"it" => "TRANS ",
+	"nl" => "TRANS ",
+	"ru" => "TRANS ",
+	"de" => "TRANS ",
+	"zh" => "TRANS ",
+	"pl" => "TRANS ",
+	"se" => "TRANS ",
+	"es" => "TRANS ",
+	"pt" => "TRANS "
+);
+
+$txt_sshsql_ = array(
+	"fr" => "",
+	"en" => "",
+	"hu" => "TRANS ",
+	"it" => "TRANS ",
+	"nl" => "TRANS ",
+	"ru" => "TRANS ",
+	"de" => "TRANS ",
+	"zh" => "TRANS ",
+	"pl" => "TRANS ",
+	"se" => "TRANS ",
+	"es" => "TRANS ",
+	"pt" => "TRANS "
+);
+
+$txt_sshsql_ = array(
+	"fr" => "",
+	"en" => "",
+	"hu" => "TRANS ",
+	"it" => "TRANS ",
+	"nl" => "TRANS ",
+	"ru" => "TRANS ",
+	"de" => "TRANS ",
+	"zh" => "TRANS ",
+	"pl" => "TRANS ",
+	"se" => "TRANS ",
+	"es" => "TRANS ",
+	"pt" => "TRANS "
+);
+
+$txt_sshsql_ = array(
+	"fr" => "",
+	"en" => "",
+	"hu" => "TRANS ",
+	"it" => "TRANS ",
+	"nl" => "TRANS ",
+	"ru" => "TRANS ",
+	"de" => "TRANS ",
+	"zh" => "TRANS ",
+	"pl" => "TRANS ",
+	"se" => "TRANS ",
+	"es" => "TRANS ",
+	"pt" => "TRANS "
+);
+
+$txt_sshsql_ = array(
+	"fr" => "",
+	"en" => "",
+	"hu" => "TRANS ",
+	"it" => "TRANS ",
+	"nl" => "TRANS ",
+	"ru" => "TRANS ",
+	"de" => "TRANS ",
+	"zh" => "TRANS ",
+	"pl" => "TRANS ",
+	"se" => "TRANS ",
+	"es" => "TRANS ",
+	"pt" => "TRANS "
+);
+
+$txt_sshsql_ = array(
+	"fr" => "",
+	"en" => "",
+	"hu" => "TRANS ",
+	"it" => "TRANS ",
+	"nl" => "TRANS ",
+	"ru" => "TRANS ",
+	"de" => "TRANS ",
+	"zh" => "TRANS ",
+	"pl" => "TRANS ",
+	"se" => "TRANS ",
+	"es" => "TRANS ",
+	"pt" => "TRANS "
+);
+
+$txt_sshsql_ = array(
+	"fr" => "",
+	"en" => "",
+	"hu" => "TRANS ",
+	"it" => "TRANS ",
+	"nl" => "TRANS ",
+	"ru" => "TRANS ",
+	"de" => "TRANS ",
+	"zh" => "TRANS ",
+	"pl" => "TRANS ",
+	"se" => "TRANS ",
+	"es" => "TRANS ",
+	"pt" => "TRANS "
+);
+
+$txt_sshsql_ = array(
+	"fr" => "",
+	"en" => "",
+	"hu" => "TRANS ",
+	"it" => "TRANS ",
+	"nl" => "TRANS ",
+	"ru" => "TRANS ",
+	"de" => "TRANS ",
+	"zh" => "TRANS ",
+	"pl" => "TRANS ",
+	"se" => "TRANS ",
+	"es" => "TRANS ",
+	"pt" => "TRANS "
+);
+
+$txt_sshsql_ = array(
+	"fr" => "",
+	"en" => "",
+	"hu" => "TRANS ",
+	"it" => "TRANS ",
+	"nl" => "TRANS ",
+	"ru" => "TRANS ",
+	"de" => "TRANS ",
+	"zh" => "TRANS ",
+	"pl" => "TRANS ",
+	"se" => "TRANS ",
+	"es" => "TRANS ",
+	"pt" => "TRANS "
+);
+
+$txt_sshsql_ = array(
+	"fr" => "",
+	"en" => "",
+	"hu" => "TRANS ",
+	"it" => "TRANS ",
+	"nl" => "TRANS ",
+	"ru" => "TRANS ",
+	"de" => "TRANS ",
+	"zh" => "TRANS ",
+	"pl" => "TRANS ",
+	"se" => "TRANS ",
+	"es" => "TRANS ",
+	"pt" => "TRANS "
+);
+
+$txt_sshsql_ = array(
+	"fr" => "",
+	"en" => "",
+	"hu" => "TRANS ",
+	"it" => "TRANS ",
+	"nl" => "TRANS ",
+	"ru" => "TRANS ",
+	"de" => "TRANS ",
+	"zh" => "TRANS ",
+	"pl" => "TRANS ",
+	"se" => "TRANS ",
+	"es" => "TRANS ",
+	"pt" => "TRANS "
+);
+
+$txt_sshsql_ = array(
+	"fr" => "",
+	"en" => "",
+	"hu" => "TRANS ",
+	"it" => "TRANS ",
+	"nl" => "TRANS ",
+	"ru" => "TRANS ",
+	"de" => "TRANS ",
+	"zh" => "TRANS ",
+	"pl" => "TRANS ",
+	"se" => "TRANS ",
+	"es" => "TRANS ",
+	"pt" => "TRANS "
+);
+
+$txt_sshsql_ = array(
+	"fr" => "",
+	"en" => "",
+	"hu" => "TRANS ",
+	"it" => "TRANS ",
+	"nl" => "TRANS ",
+	"ru" => "TRANS ",
+	"de" => "TRANS ",
+	"zh" => "TRANS ",
+	"pl" => "TRANS ",
+	"se" => "TRANS ",
+	"es" => "TRANS ",
+	"pt" => "TRANS "
+);
+
+$txt_sshsql_ = array(
+	"fr" => "",
+	"en" => "",
+	"hu" => "TRANS ",
+	"it" => "TRANS ",
+	"nl" => "TRANS ",
+	"ru" => "TRANS ",
+	"de" => "TRANS ",
+	"zh" => "TRANS ",
+	"pl" => "TRANS ",
+	"se" => "TRANS ",
+	"es" => "TRANS ",
+	"pt" => "TRANS "
+);
+
+$txt_sshsql_ = array(
+	"fr" => "",
+	"en" => "",
+	"hu" => "TRANS ",
+	"it" => "TRANS ",
+	"nl" => "TRANS ",
+	"ru" => "TRANS ",
+	"de" => "TRANS ",
+	"zh" => "TRANS ",
+	"pl" => "TRANS ",
+	"se" => "TRANS ",
+	"es" => "TRANS ",
+	"pt" => "TRANS "
+);
+
+$txt_sshsql_ = array(
+	"fr" => "",
+	"en" => "",
+	"hu" => "TRANS ",
+	"it" => "TRANS ",
+	"nl" => "TRANS ",
+	"ru" => "TRANS ",
+	"de" => "TRANS ",
+	"zh" => "TRANS ",
+	"pl" => "TRANS ",
+	"se" => "TRANS ",
+	"es" => "TRANS ",
+	"pt" => "TRANS "
+);
+
+$txt_sshsql_ = array(
+	"fr" => "",
+	"en" => "",
+	"hu" => "TRANS ",
+	"it" => "TRANS ",
+	"nl" => "TRANS ",
+	"ru" => "TRANS ",
+	"de" => "TRANS ",
+	"zh" => "TRANS ",
+	"pl" => "TRANS ",
+	"se" => "TRANS ",
+	"es" => "TRANS ",
+	"pt" => "TRANS "
+);
+
+$txt_sshsql_ = array(
+	"fr" => "",
+	"en" => "",
+	"hu" => "TRANS ",
+	"it" => "TRANS ",
+	"nl" => "TRANS ",
+	"ru" => "TRANS ",
+	"de" => "TRANS ",
+	"zh" => "TRANS ",
+	"pl" => "TRANS ",
+	"se" => "TRANS ",
+	"es" => "TRANS ",
+	"pt" => "TRANS "
+);
+
+$txt_sshsql_ = array(
+	"fr" => "",
+	"en" => "",
+	"hu" => "TRANS ",
+	"it" => "TRANS ",
+	"nl" => "TRANS ",
+	"ru" => "TRANS ",
+	"de" => "TRANS ",
+	"zh" => "TRANS ",
+	"pl" => "TRANS ",
+	"se" => "TRANS ",
+	"es" => "TRANS ",
+	"pt" => "TRANS "
+);
+
+$txt_sshsql_ = array(
+	"fr" => "",
+	"en" => "",
+	"hu" => "TRANS ",
+	"it" => "TRANS ",
+	"nl" => "TRANS ",
+	"ru" => "TRANS ",
+	"de" => "TRANS ",
+	"zh" => "TRANS ",
+	"pl" => "TRANS ",
+	"se" => "TRANS ",
+	"es" => "TRANS ",
+	"pt" => "TRANS "
+);
+
+$txt_sshsql_ = array(
+	"fr" => "",
+	"en" => "",
+	"hu" => "TRANS ",
+	"it" => "TRANS ",
+	"nl" => "TRANS ",
+	"ru" => "TRANS ",
+	"de" => "TRANS ",
+	"zh" => "TRANS ",
+	"pl" => "TRANS ",
+	"se" => "TRANS ",
+	"es" => "TRANS ",
+	"pt" => "TRANS "
+);
+
+$txt_sshsql_ = array(
+	"fr" => "",
+	"en" => "",
+	"hu" => "TRANS ",
+	"it" => "TRANS ",
+	"nl" => "TRANS ",
+	"ru" => "TRANS ",
+	"de" => "TRANS ",
+	"zh" => "TRANS ",
+	"pl" => "TRANS ",
+	"se" => "TRANS ",
+	"es" => "TRANS ",
+	"pt" => "TRANS "
+);
+
+$txt_sshsql_ = array(
+	"fr" => "",
+	"en" => "",
+	"hu" => "TRANS ",
+	"it" => "TRANS ",
+	"nl" => "TRANS ",
+	"ru" => "TRANS ",
+	"de" => "TRANS ",
+	"zh" => "TRANS ",
+	"pl" => "TRANS ",
+	"se" => "TRANS ",
+	"es" => "TRANS ",
+	"pt" => "TRANS "
+);
+
+$txt_sshsql_ = array(
+	"fr" => "",
+	"en" => "",
+	"hu" => "TRANS ",
+	"it" => "TRANS ",
+	"nl" => "TRANS ",
+	"ru" => "TRANS ",
+	"de" => "TRANS ",
+	"zh" => "TRANS ",
+	"pl" => "TRANS ",
+	"se" => "TRANS ",
+	"es" => "TRANS ",
+	"pt" => "TRANS "
+);
+
+$txt_sshsql_ = array(
+	"fr" => "",
+	"en" => "",
+	"hu" => "TRANS ",
+	"it" => "TRANS ",
+	"nl" => "TRANS ",
+	"ru" => "TRANS ",
+	"de" => "TRANS ",
+	"zh" => "TRANS ",
+	"pl" => "TRANS ",
+	"se" => "TRANS ",
+	"es" => "TRANS ",
+	"pt" => "TRANS "
+);
+
+$txt_sshsql_ = array(
+	"fr" => "",
+	"en" => "",
+	"hu" => "TRANS ",
+	"it" => "TRANS ",
+	"nl" => "TRANS ",
+	"ru" => "TRANS ",
+	"de" => "TRANS ",
+	"zh" => "TRANS ",
+	"pl" => "TRANS ",
+	"se" => "TRANS ",
+	"es" => "TRANS ",
+	"pt" => "TRANS "
+);
+
+$txt_sshsql_ = array(
+	"fr" => "",
+	"en" => "",
+	"hu" => "TRANS ",
+	"it" => "TRANS ",
+	"nl" => "TRANS ",
+	"ru" => "TRANS ",
+	"de" => "TRANS ",
+	"zh" => "TRANS ",
+	"pl" => "TRANS ",
+	"se" => "TRANS ",
+	"es" => "TRANS ",
+	"pt" => "TRANS "
+);
+
+$txt_sshsql_ = array(
+	"fr" => "",
+	"en" => "",
+	"hu" => "TRANS ",
+	"it" => "TRANS ",
+	"nl" => "TRANS ",
+	"ru" => "TRANS ",
+	"de" => "TRANS ",
+	"zh" => "TRANS ",
+	"pl" => "TRANS ",
+	"se" => "TRANS ",
+	"es" => "TRANS ",
+	"pt" => "TRANS "
+);
+
+$txt_sshsql_ = array(
+	"fr" => "",
+	"en" => "",
+	"hu" => "TRANS ",
+	"it" => "TRANS ",
+	"nl" => "TRANS ",
+	"ru" => "TRANS ",
+	"de" => "TRANS ",
+	"zh" => "TRANS ",
+	"pl" => "TRANS ",
+	"se" => "TRANS ",
+	"es" => "TRANS ",
+	"pt" => "TRANS "
+);
+
+$txt_sshsql_ = array(
+	"fr" => "",
+	"en" => "",
+	"hu" => "TRANS ",
+	"it" => "TRANS ",
+	"nl" => "TRANS ",
+	"ru" => "TRANS ",
+	"de" => "TRANS ",
+	"zh" => "TRANS ",
+	"pl" => "TRANS ",
+	"se" => "TRANS ",
+	"es" => "TRANS ",
+	"pt" => "TRANS "
+);
+
+?>
