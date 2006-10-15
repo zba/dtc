e8f5ed4500c41d2f440f34bf6be84c83f85e215a
thomas <thomas>
2006-10-15 11:34:02 +0000

    A warning message saying it's not ready yet.

--- shared/inc/forms/ticket.php
+++ shared/inc/forms/ticket.php
@@ -9,7 +9,7 @@ function drawTickets($admin){
 	global $addrlink;
 	global $conf_administrative_site;
 
-	$out = "<br>";
+	$out = "<font color=\"red\">This part is still in development: do not use</font><br>";
 
 	if(isset($_REQUEST["subaction"]) && $_REQUEST["subaction"] == "new_ticket"){
 		$popup_hostname = "";
