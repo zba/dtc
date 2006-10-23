98575a2f19851c84635943d4572eea36982bd942
tusker <tusker>
2006-10-23 03:52:36 +0000

    Bump bytes_sent and bytes_receive to bigint unsigned

--- admin/tables/http_accounting.sql
+++ admin/tables/http_accounting.sql
@@ -4,7 +4,8 @@
 CREATE TABLE IF NOT EXISTS http_accounting (
   id int(14) NOT NULL auto_increment,
   vhost varchar(50) NOT NULL default '',
-  bytes_sent int(14) NOT NULL default '0',
+  bytes_sent bigint(14) unsigned NOT NULL default '0',
+  bytes_receive bigint(14) unsigned NOT NULL default '0',
   count_hosts int(12) NOT NULL default '0',
   count_visits int(12) NOT NULL default '0',
   count_status_200 int(12) NOT NULL default '0',
