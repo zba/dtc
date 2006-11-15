dd3d201eeaf330957832e8b347210a72e9f97e62
thomas <thomas>
2006-11-15 08:09:17 +0000

    Somme corrections...

--- admin/tables/ssh_groups.sql
+++ admin/tables/ssh_groups.sql
@@ -1,4 +1,4 @@
-CREATE TABLE IF NOT EXISTS `nss_groups` (
+CREATE TABLE IF NOT EXISTS `ssh_groups` (
   `group_id` int(11) NOT NULL auto_increment,
   `group_name` varchar(30) NOT NULL default '',
   `status` char(1) default 'A',
