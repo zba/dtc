f4ce33140d6873d93d7c863e9b63827f3c4c5e01
thomas <thomas>
2007-01-06 04:36:46 +0000

    And re woooooooooops!

--- admin/genfiles/gen_pro_vhost.php
+++ admin/genfiles/gen_pro_vhost.php
@@ -7,8 +7,8 @@ $chk_dir_script="#!/bin/sh
 
 echo \"Checking vhosts directories existance...\"\n";
 function vhost_chk_dir_sh($dir){
-	global $dtc_system_username;
-	global $dtc_system_groupname;
+	global $conf_dtc_system_username;
+	global $conf_dtc_system_groupname;
 
 	global $chk_dir_script;
 
@@ -32,7 +32,7 @@ fi
 	$chk_dir_script .= "
 if [ ! -d $dir ] ; then
 	mkdir -p $dir
-	chown $dtc_system_username:$dtc_system_groupname
+	chown $conf_dtc_system_username:$conf_dtc_system_groupname
 	echo \"Directory $dir was missing and has been created.\"
 fi
 ";
