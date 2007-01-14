719a618aa07ac10f9d11a1be4a8c68739c24e651
thomas <thomas>
2007-01-14 09:07:06 +0000

    Added dedicated servers to renewals.

--- admin/reminders.php
+++ admin/reminders.php
@@ -117,6 +117,86 @@ sendVPSReminderEmail(-$conf_vps_renewal_lastwarning,"vps_expired_last_warning.tx
 // Send the shutdown message
 sendVPSReminderEmail(-$conf_vps_renewal_shutdown,"vps_expired_shutdown.txt","yes");
 
+// Send all the mail for dedicated server for a given renew period
+function sendDedicatedReminderEmail($remaining_days,$file,$send_webmaster_copy="no"){
+	global $pro_mysql_admin_table;
+	global $pro_mysql_client_table;
+	global $pro_mysql_dedicated_table;
+	global $conf_webmaster_email_addr;
+
+	$fname = "reminders_msg/".$file;
+	$fp = fopen($fname,"r");
+	$mesg = fread($fp,filesize($fname));
+	fclose($fp);
+
+	$now_timestamp = mktime();
+	$one_day = 3600 * 24;
+	$q = "SELECT * FROM $pro_mysql_dedicated_table WHERE expire_date='".date("Y-m-d",$now_timestamp + $one_day*$remaining_days)."';";
+	$r = mysql_query($q)or die("Cannot query $q line ".__LINE__." file ".__FILE__." sql said: ".mysql_error());
+	$n = mysql_num_rows($r);
+	for($i=0;$i<$n;$i++){
+		$dedicated = mysql_fetch_array($r);
+
+		// Get the admin
+		$q2 = "SELECT * FROM $pro_mysql_admin_table WHERE adm_login='".$dedicated["owner"]."';";
+		$r2 = mysql_query($q2)or die("Cannot query $q2 line ".__LINE__." file ".__FILE__." sql said: ".mysql_error());
+		$n2 = mysql_num_rows($r2);
+		if($n2 != 1){
+			sendAdminWarning("Could not get admin_login ".$dedicated["owner"]." line ".__LINE__." file ".__FILE__);
+			continue;
+		}
+		$admin = mysql_fetch_array($r2);
+		if($admin["id_client"] == 0){
+			sendAdminWarning("Admin has no client id (".$admin["id_client"].") line ".__LINE__." file ".__FILE__);
+			continue;
+		}
+
+		// Get the client
+		$q2 = "SELECT * FROM $pro_mysql_client_table WHERE id='".$admin["id_client"]."';";
+		$r2 = mysql_query($q2)or die("Cannot query $q2 line ".__LINE__." file ".__FILE__." sql said: ".mysql_error());
+		$n2 = mysql_num_rows($r2);
+		if($n2 != 1){
+			sendAdminWarning("Could not find id client ".$admin["id_client"]." line ".__LINE__." file ".__FILE__);
+			continue;
+		}
+		$client = mysql_fetch_array($r2);
+
+		// Write the email
+		$msg_2_send = $mesg;
+		$msg_2_send = getCustomizedReminder($msg_2_send,$client["christname"],$remaining_days,$dedicated["expire_date"],$admin["adm_login"]);
+		$msg_2_send = str_replace("%%%SERVER_HOSTNAME%%%",$dedicated["server_hostname"],$msg_2_send);
+
+		$headers = "From: ".$conf_webmaster_email_addr;
+		mail($client["email"],"[DTC] Your dedicated server expiration",$msg_2_send,$headers);
+		if($send_webmaster_copy == "yes"){
+			mail($conf_webmaster_email_addr,"[DTC] A dedicated has expired",$msg_2_send,$headers);
+		}
+	}
+}
+
+//////////////////////////////////
+// DEDICATED SERVERS EXPIRATION //
+//////////////////////////////////
+// Send reminders before expiration
+$before = explode("|",$conf_vps_renewal_before);
+$n = sizeof($before);
+for($i=0;$i<$n;$i++){
+	sendDedicatedReminderEmail($before[$i],"server_will_expire.txt");
+}
+// Send reminders the day of the expiration
+sendDedicatedReminderEmail(0,"server_expired_today.txt","yes");
+// Send reminders after expiration
+$after = explode("|",$conf_vps_renewal_after);
+$n = sizeof($after);
+for($i=0;$i<$n;$i++){
+	$days = 0 - $after[$i];
+	sendDedicatedReminderEmail($days,"server_expired_already.txt");
+}
+// Send reminders for last warning
+sendDedicatedReminderEmail(-$conf_vps_renewal_lastwarning,"server_expired_last_warning.txt","yes");
+// Send the shutdown message
+sendDedicatedReminderEmail(-$conf_vps_renewal_shutdown,"server_expired_shutdown.txt","yes");
+
 ///////////////////////////////
 // SHARED HOSTING EXPIRATION //
 ///////////////////////////////
