bb031c8cd1e57ecc3972a9351a5a5aa2651e7af7
thomas <thomas>
2007-01-26 07:59:22 +0000

    Forgot to add the invoicing table ! :( Silly me...

---
+++ admin/tables/invoicing.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS invoicing (
+  id int(12) NOT NULL auto_increment,
+  customer_country_code char(2) NOT NULL default '',
+  service_country_code char(2) NOT NULL default '',
+  company_id int(12) NOT NULL default '0',
+  PRIMARY KEY  (id)
+) TYPE=MyISAM;
