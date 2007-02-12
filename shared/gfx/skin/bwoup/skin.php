920d7949cfb6ef2925379135a3e576957c65b67f
thomas <thomas>
2007-02-12 07:27:52 +0000

    Even better now.

--- shared/gfx/skin/bwoup/skin.php
+++ shared/gfx/skin/bwoup/skin.php
@@ -6,18 +6,16 @@ $skinTable[] = array(
 	"functionCode" => '
 	$skinpath = "$skinGeneralPath/$skinpath";
 
-	return "<table class=\"box_wnb\" border=\"0\" cellpadding=\"0\" cellspacing=\"0\">
-<tr>
-<td class=\"box_wnb_nb\" valign=\"top\">
-<div class=\"box_wnb_nb_title\">
-<div class=\"box_wnb_nb_title_left\">
-<div class=\"box_wnb_nb_title_right\">
-<div class=\"box_wnb_nb_title_mid\">$title</div>
-</div>
-</div>
-</div>
-<div style=\"padding-left:4px; padding-right:4px; padding-top:4px; padding-bottom:4px\">$content</div>
-</td></tr></table>";'
+	return "<div class=\"box\">
+	  <div class=\"box_topright\"><div class=\"box_bottomright\"><div class=\"box_bottomleft\"><div class=\"box_topleft\">
+	  <div class=\"box_content\">
+
+
+	  <h2>$title</h2>
+	  $content
+	</div></div></div></div>
+</div></div>
+";'
 	);
 
 ?>
