99cbd695353e2fca2b14c318057c74922ef7aa39
thomas <thomas>
2007-02-15 09:22:32 +0000

    Forgot this file.

---
+++ admin/inc/draw_user_admin_strings.php
@@ -0,0 +1,468 @@
+<?php
+
+$txt_dua_no_pending_renewals =array(
+	"fr" => "Pas de renouvellements en attente!",
+	"en" => "No pending renewals!",
+	"hu" => "TRANS No pending renewals!",
+	"it" => "TRANS No pending renewals!",
+	"nl" => "TRANS No pending renewals!",
+	"ru" => "TRANS No pending renewals!",
+	"de" => "TRANS No pending renewals!",
+	"zh" => "TRANS No pending renewals!",
+	"pl" => "TRANS No pending renewals!",
+	"se" => "TRANS No pending renewals!",
+	"es" => "TRANS No pending renewals!",
+	"pt" => "TRANS No pending renewals!"
+	);
+
+$txt_dua_product = array(
+	"fr" => "Produit",
+	"en" => "Product",
+	"hu" => "TRANS Product",
+	"it" => "TRANS Product",
+	"nl" => "TRANS Product",
+	"ru" => "TRANS Product",
+	"de" => "TRANS Product",
+	"zh" => "TRANS Product",
+	"pl" => "TRANS Product",
+	"se" => "TRANS Product",
+	"es" => "TRANS Product",
+	"pt" => "TRANS Product"
+	);
+
+$txt_dua_payment_date = array(
+	"fr" => "Date de paiement",
+	"en" => "Payment date",
+	"hu" => "TRANS Payment date",
+	"it" => "TRANS Payment date",
+	"nl" => "TRANS Payment date",
+	"ru" => "TRANS Payment date",
+	"de" => "TRANS Payment date",
+	"zh" => "TRANS Payment date",
+	"pl" => "TRANS Payment date",
+	"se" => "TRANS Payment date",
+	"es" => "TRANS Payment date",
+	"pt" => "TRANS Payment date"
+	);
+
+$txt_dua_bank_validated = array(
+	"fr" => "Validation banque",
+	"en" => "Bank validated",
+	"hu" => "TRANS Bank validated",
+	"it" => "TRANS Bank validated",
+	"nl" => "TRANS Bank validated",
+	"ru" => "TRANS Bank validated",
+	"de" => "TRANS Bank validated",
+	"zh" => "TRANS Bank validated",
+	"pl" => "TRANS Bank validated",
+	"se" => "TRANS Bank validated",
+	"es" => "TRANS Bank validated",
+	"pt" => "TRANS Bank validated"
+	);
+
+$txt_dua_type = array(
+	"fr" => "Type",
+	"en" => "Type",
+	"hu" => "TRANS Type",
+	"it" => "TRANS Type",
+	"nl" => "TRANS Type",
+	"ru" => "TRANS Type",
+	"de" => "TRANS Type",
+	"zh" => "TRANS Type",
+	"pl" => "TRANS Type",
+	"se" => "TRANS Type",
+	"es" => "TRANS Type",
+	"pt" => "TRANS Type"
+	);
+
+$txt_dua_type2 = array(
+	"fr" => "Type : ",
+	"en" => "Type: ",
+	"hu" => "TRANS Type: ",
+	"it" => "TRANS Type: ",
+	"nl" => "TRANS Type: ",
+	"ru" => "TRANS Type: ",
+	"de" => "TRANS Type: ",
+	"zh" => "TRANS Type: ",
+	"pl" => "TRANS Type: ",
+	"se" => "TRANS Type: ",
+	"es" => "TRANS Type: ",
+	"pt" => "TRANS Type: "
+	);
+
+$txt_dua_cannot_find_payment = array(
+	"fr" => "Impossible de trouver le paiement !",
+	"en" => "Cannot find payment!",
+	"hu" => "TRANS Cannot find payment!",
+	"it" => "TRANS Cannot find payment!",
+	"nl" => "TRANS Cannot find payment!",
+	"ru" => "TRANS Cannot find payment!",
+	"de" => "TRANS Cannot find payment!",
+	"zh" => "TRANS Cannot find payment!",
+	"pl" => "TRANS Cannot find payment!",
+	"se" => "TRANS Cannot find payment!",
+	"es" => "TRANS Cannot find payment!",
+	"pt" => "TRANS Cannot find payment!"
+	);
+
+$txt_dua_cannot_find_vps_in_db = array(
+	"fr" => "Impossible de trouver le VDS dans la base !",
+	"en" => "VPS: Cannot find VPS in db!",
+	"hu" => "TRANS VPS: Cannot find VPS in db!",
+	"it" => "TRANS VPS: Cannot find VPS in db!",
+	"nl" => "TRANS VPS: Cannot find VPS in db!",
+	"ru" => "TRANS VPS: Cannot find VPS in db!",
+	"de" => "TRANS VPS: Cannot find VPS in db!",
+	"zh" => "TRANS VPS: Cannot find VPS in db!",
+	"pl" => "TRANS VPS: Cannot find VPS in db!",
+	"se" => "TRANS VPS: Cannot find VPS in db!",
+	"es" => "TRANS VPS: Cannot find VPS in db!",
+	"pt" => "TRANS VPS: Cannot find VPS in db!"
+	);
+
+$txt_dua_shared = array(
+	"fr" => "Shared",
+	"en" => "Shared",
+	"hu" => "TRANS Shared",
+	"it" => "TRANS Shared",
+	"nl" => "TRANS Shared",
+	"ru" => "TRANS Shared",
+	"de" => "TRANS Shared",
+	"zh" => "TRANS Shared",
+	"pl" => "TRANS Shared",
+	"se" => "TRANS Shared",
+	"es" => "TRANS Shared",
+	"pt" => "TRANS Shared"
+	);
+
+$txt_dua_ssl_token_purchase = array(
+	"fr" => "Achat d'un jetton SSL",
+	"en" => "SSL Token purchase",
+	"hu" => "TRANS SSL Token purchase",
+	"it" => "TRANS SSL Token purchase",
+	"nl" => "TRANS SSL Token purchase",
+	"ru" => "TRANS SSL Token purchase",
+	"de" => "TRANS SSL Token purchase",
+	"zh" => "TRANS SSL Token purchase",
+	"pl" => "TRANS SSL Token purchase",
+	"se" => "TRANS SSL Token purchase",
+	"es" => "TRANS SSL Token purchase",
+	"pt" => "TRANS SSL Token purchase"
+	);
+
+$txt_dua_ssl_token_renewal = array(
+	"fr" => "Renouvellement jetton SSL",
+	"en" => "SSL Token renewal",
+	"hu" => "TRANS SSL Token renewal",
+	"it" => "TRANS SSL Token renewal",
+	"nl" => "TRANS SSL Token renewal",
+	"ru" => "TRANS SSL Token renewal",
+	"de" => "TRANS SSL Token renewal",
+	"zh" => "TRANS SSL Token renewal",
+	"pl" => "TRANS SSL Token renewal",
+	"se" => "TRANS SSL Token renewal",
+	"es" => "TRANS SSL Token renewal",
+	"pt" => "TRANS SSL Token renewal"
+	);
+
+$txt_dua_cannot_find_server = array(
+	"fr" => "impossible de trouver le serveur dans la base!",
+	"en" => "cannot find server in db!",
+	"hu" => "TRANS cannot find server in db!",
+	"it" => "TRANS cannot find server in db!",
+	"nl" => "TRANS cannot find server in db!",
+	"ru" => "TRANS cannot find server in db!",
+	"de" => "TRANS cannot find server in db!",
+	"zh" => "TRANS cannot find server in db!",
+	"pl" => "TRANS cannot find server in db!",
+	"se" => "TRANS cannot find server in db!",
+	"es" => "TRANS cannot find server in db!",
+	"pt" => "TRANS cannot find server in db!"
+	);
+
+$txt_dua_server = array(
+	"fr" => "Serveur : ",
+	"en" => "Server: ",
+	"hu" => "TRANS Server: ",
+	"it" => "TRANS Server: ",
+	"nl" => "TRANS Server: ",
+	"ru" => "TRANS Server: ",
+	"de" => "TRANS Server: ",
+	"zh" => "TRANS Server: ",
+	"pl" => "TRANS Server: ",
+	"se" => "TRANS Server: ",
+	"es" => "TRANS Server: ",
+	"pt" => "TRANS Server: "
+	);
+
+$txt_dua_no_pending_support_tickets = array(
+	"fr" => "Pas de tiquet de support en attente !",
+	"en" => "No pending support tickets!",
+	"hu" => "TRANS No pending support tickets!",
+	"it" => "TRANS No pending support tickets!",
+	"nl" => "TRANS No pending support tickets!",
+	"ru" => "TRANS No pending support tickets!",
+	"de" => "TRANS No pending support tickets!",
+	"zh" => "TRANS No pending support tickets!",
+	"pl" => "TRANS No pending support tickets!",
+	"se" => "TRANS No pending support tickets!",
+	"es" => "TRANS No pending support tickets!",
+	"pt" => "TRANS No pending support tickets!"
+	);
+
+$txt_dua_age = array(
+	"fr" => "Age",
+	"en" => "Age",
+	"hu" => "TRANS Age",
+	"it" => "TRANS Age",
+	"nl" => "TRANS Age",
+	"ru" => "TRANS Age",
+	"de" => "TRANS Age",
+	"zh" => "TRANS Age",
+	"pl" => "TRANS Age",
+	"se" => "TRANS Age",
+	"es" => "TRANS Age",
+	"pt" => "TRANS Age"
+	);
+
+$txt_dua_subject = array(
+	"fr" => "Sujet",
+	"en" => "Subject",
+	"hu" => "TRANS Subject",
+	"it" => "TRANS Subject",
+	"nl" => "TRANS Subject",
+	"ru" => "TRANS Subject",
+	"de" => "TRANS Subject",
+	"zh" => "TRANS Subject",
+	"pl" => "TRANS Subject",
+	"se" => "TRANS Subject",
+	"es" => "TRANS Subject",
+	"pt" => "TRANS Subject"
+	);
+
+$txt_dua_subject2 = array(
+	"fr" => "Suject : ",
+	"en" => "Subject: ",
+	"hu" => "TRANS Subject: ",
+	"it" => "TRANS Subject: ",
+	"nl" => "TRANS Subject: ",
+	"ru" => "TRANS Subject: ",
+	"de" => "TRANS Subject: ",
+	"zh" => "TRANS Subject: ",
+	"pl" => "TRANS Subject: ",
+	"se" => "TRANS Subject: ",
+	"es" => "TRANS Subject: ",
+	"pt" => "TRANS Subject: "
+	);
+
+$txt_dua_cannot_find_ticket = array(
+	"fr" => "Impossible de trouver le tiquet !",
+	"en" => "Cannot find ticket!",
+	"hu" => "TRANS Cannot find ticket!",
+	"it" => "TRANS Cannot find ticket!",
+	"nl" => "TRANS Cannot find ticket!",
+	"ru" => "TRANS Cannot find ticket!",
+	"de" => "TRANS Cannot find ticket!",
+	"zh" => "TRANS Cannot find ticket!",
+	"pl" => "TRANS Cannot find ticket!",
+	"se" => "TRANS Cannot find ticket!",
+	"es" => "TRANS Cannot find ticket!",
+	"pt" => "TRANS Cannot find ticket!"
+	);
+
+$txt_dua_ticket_type_not_found = array(
+	"fr" => "type non trouvé !",
+	"en" => "type not found!",
+	"hu" => "TRANS type not found!",
+	"it" => "TRANS type not found!",
+	"nl" => "TRANS type not found!",
+	"ru" => "TRANS type not found!",
+	"de" => "TRANS type not found!",
+	"zh" => "TRANS type not found!",
+	"pl" => "TRANS type not found!",
+	"se" => "TRANS type not found!",
+	"es" => "TRANS type not found!",
+	"pt" => "TRANS type not found!"
+	);
+
+$txt_dua_request_to_close_the_ticket = array(
+	"fr" => "Demander de fermer le tiquet : ",
+	"en" => "Request to close the ticket: ",
+	"hu" => "TRANS Request to close the ticket: ",
+	"it" => "TRANS Request to close the ticket: ",
+	"nl" => "TRANS Request to close the ticket: ",
+	"ru" => "TRANS Request to close the ticket: ",
+	"de" => "TRANS Request to close the ticket: ",
+	"zh" => "TRANS Request to close the ticket: ",
+	"pl" => "TRANS Request to close the ticket: ",
+	"se" => "TRANS Request to close the ticket: ",
+	"es" => "TRANS Request to close the ticket: ",
+	"pt" => "TRANS Request to close the ticket: "
+	);
+
+$txt_dua_back_to_pending_requests = array(
+	"fr" => "Retour au demandes en attente",
+	"en" => "Back to pending requests",
+	"hu" => "TRANS Back to pending requests",
+	"it" => "TRANS Back to pending requests",
+	"nl" => "TRANS Back to pending requests",
+	"ru" => "TRANS Back to pending requests",
+	"de" => "TRANS Back to pending requests",
+	"zh" => "TRANS Back to pending requests",
+	"pl" => "TRANS Back to pending requests",
+	"se" => "TRANS Back to pending requests",
+	"es" => "TRANS Back to pending requests",
+	"pt" => "TRANS Back to pending requests"
+	);
+
+$txt_dua_name = array(
+	"fr" => "Name",
+	"en" => "Name",
+	"hu" => "TRANS Name",
+	"it" => "TRANS Name",
+	"nl" => "TRANS Name",
+	"ru" => "TRANS Name",
+	"de" => "TRANS Name",
+	"zh" => "TRANS Name",
+	"pl" => "TRANS Name",
+	"se" => "TRANS Name",
+	"es" => "TRANS Name",
+	"pt" => "TRANS Name"
+	);
+
+$txt_dua_domain_name_vps_server_hostname = array(
+	"fr" => "Nom de domaine / Nom d'hote du serveur VPS",
+	"en" => "Domain name / VPS server hostname",
+	"hu" => "TRANS Domain name / VPS server hostname",
+	"it" => "TRANS Domain name / VPS server hostname",
+	"nl" => "TRANS Domain name / VPS server hostname",
+	"ru" => "TRANS Domain name / VPS server hostname",
+	"de" => "TRANS Domain name / VPS server hostname",
+	"zh" => "TRANS Domain name / VPS server hostname",
+	"pl" => "TRANS Domain name / VPS server hostname",
+	"se" => "TRANS Domain name / VPS server hostname",
+	"es" => "TRANS Domain name / VPS server hostname",
+	"pt" => "TRANS Domain name / VPS server hostname"
+	);
+
+$txt_dua_send_reply = array(
+	"fr" => "Envoyer réponce",
+	"en" => "Send reply",
+	"hu" => "TRANS Send reply",
+	"it" => "TRANS Send reply",
+	"nl" => "TRANS Send reply",
+	"ru" => "TRANS Send reply",
+	"de" => "TRANS Send reply",
+	"zh" => "TRANS Send reply",
+	"pl" => "TRANS Send reply",
+	"se" => "TRANS Send reply",
+	"es" => "TRANS Send reply",
+	"pt" => "TRANS Send reply"
+	);
+
+$txt_dua_send_reply_and_close_ticket = array(
+	"fr" => "Send reply and close ticket",
+	"en" => "Send reply and close ticket",
+	"hu" => "TRANS Send reply and close ticket",
+	"it" => "TRANS Send reply and close ticket",
+	"nl" => "TRANS Send reply and close ticket",
+	"ru" => "TRANS Send reply and close ticket",
+	"de" => "TRANS Send reply and close ticket",
+	"zh" => "TRANS Send reply and close ticket",
+	"pl" => "TRANS Send reply and close ticket",
+	"se" => "TRANS Send reply and close ticket",
+	"es" => "TRANS Send reply and close ticket",
+	"pt" => "TRANS Send reply and close ticket"
+	);
+
+$txt_dua_close_without_reply = array(
+	"fr" => "Close without reply",
+	"en" => "Close without reply",
+	"hu" => "TRANS Close without reply",
+	"it" => "TRANS Close without reply",
+	"nl" => "TRANS Close without reply",
+	"ru" => "TRANS Close without reply",
+	"de" => "TRANS Close without reply",
+	"zh" => "TRANS Close without reply",
+	"pl" => "TRANS Close without reply",
+	"se" => "TRANS Close without reply",
+	"es" => "TRANS Close without reply",
+	"pt" => "TRANS Close without reply"
+	);
+
+$txt_dua_cannot_find_product_in_db = array(
+	"fr" => "Impossible de trouver le produit dans la base !",
+	"en" => "Cannot find product in db!",
+	"hu" => "TRANS Cannot find product in db!",
+	"it" => "TRANS Cannot find product in db!",
+	"nl" => "TRANS Cannot find product in db!",
+	"ru" => "TRANS Cannot find product in db!",
+	"de" => "TRANS Cannot find product in db!",
+	"zh" => "TRANS Cannot find product in db!",
+	"pl" => "TRANS Cannot find product in db!",
+	"se" => "TRANS Cannot find product in db!",
+	"es" => "TRANS Cannot find product in db!",
+	"pt" => "TRANS Cannot find product in db!"
+	);
+
+$txt_dua_del = array(
+	"fr" => "Effacer",
+	"en" => "Delete",
+	"hu" => "TRANS Delete",
+	"it" => "TRANS Delete",
+	"nl" => "TRANS Delete",
+	"ru" => "TRANS Delete",
+	"de" => "TRANS Delete",
+	"zh" => "TRANS Delete",
+	"pl" => "TRANS Delete",
+	"se" => "TRANS Delete",
+	"es" => "TRANS Delete",
+	"pt" => "TRANS Delete"
+	);
+
+$txt_dua_add = array(
+	"fr" => "Ajouter",
+	"en" => "Add",
+	"hu" => "TRANS Add",
+	"it" => "TRANS Add",
+	"nl" => "TRANS Add",
+	"ru" => "TRANS Add",
+	"de" => "TRANS Add",
+	"zh" => "TRANS Add",
+	"pl" => "TRANS Add",
+	"se" => "TRANS Add",
+	"es" => "TRANS Add",
+	"pt" => "TRANS Add"
+	);
+
+$txt_dua_view_details = array(
+	"fr" => "Voir détails",
+	"en" => "View details",
+	"hu" => "TRANS View details",
+	"it" => "TRANS View details",
+	"nl" => "TRANS View details",
+	"ru" => "TRANS View details",
+	"de" => "TRANS View details",
+	"zh" => "TRANS View details",
+	"pl" => "TRANS View details",
+	"se" => "TRANS View details",
+	"es" => "TRANS View details",
+	"pt" => "TRANS View details"
+	);
+
+$txt_dua_ = array(
+	"fr" => "",
+	"en" => "",
+	"hu" => "TRANS ",
+	"it" => "TRANS ",
+	"nl" => "TRANS ",
+	"ru" => "TRANS ",
+	"de" => "TRANS ",
+	"zh" => "TRANS ",
+	"pl" => "TRANS ",
+	"se" => "TRANS ",
+	"es" => "TRANS ",
+	"pt" => "TRANS "
+	);
+
+?>
