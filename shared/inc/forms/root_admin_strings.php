e882035ff93218fd44a9605c565c44c0358f55ae
thomas <thomas>
2007-02-15 09:23:27 +0000

    Hop!

---
+++ shared/inc/forms/root_admin_strings.php
@@ -0,0 +1,423 @@
+<?php
+
+$txt_root_admin_configuraiton_of_the_domains = array(
+	"fr" => "Cnfiguration des domaines",
+	"en" => "Configuration of the domains",
+	"hu" => "TRANS Configuration of the domains",
+	"it" => "TRANS Configuration of the domains",
+	"nl" => "TRANS Configuration of the domains",
+	"ru" => "TRANS Configuration of the domains",
+	"de" => "TRANS Configuration of the domains",
+	"zh" => "TRANS Configuration of the domains",
+	"pl" => "TRANS Configuration of the domains",
+	"se" => "TRANS Configuration of the domains",
+	"es" => "TRANS Configuration of the domains",
+	"pt" => "TRANS Configuration of the domains"
+	);
+
+$txt_root_admin_max_ssh = array(
+	"fr" => "Max SSH",
+	"en" => "Max SSH",
+	"hu" => "TRANS Max SSH",
+	"it" => "TRANS Max SSH",
+	"nl" => "TRANS Max SSH",
+	"ru" => "TRANS Max SSH",
+	"de" => "TRANS Max SSH",
+	"zh" => "TRANS Max SSH",
+	"pl" => "TRANS Max SSH",
+	"se" => "TRANS Max SSH",
+	"es" => "TRANS Max SSH",
+	"pt" => "TRANS Max SSH"
+	);
+
+$txt_root_admin_cgi_bin_protection = array(
+	"fr" => "Protection CGI-BIN",
+	"en" => "CGI-BIN protection",
+	"hu" => "TRANS CGI-BIN protection",
+	"it" => "TRANS CGI-BIN protection",
+	"nl" => "TRANS CGI-BIN protection",
+	"ru" => "TRANS CGI-BIN protection",
+	"de" => "TRANS CGI-BIN protection",
+	"zh" => "TRANS CGI-BIN protection",
+	"pl" => "TRANS CGI-BIN protection",
+	"se" => "TRANS CGI-BIN protection",
+	"es" => "TRANS CGI-BIN protection",
+	"pt" => "TRANS CGI-BIN protection"
+	);
+
+$txt_root_admin_configuration_of_the_vpses = array(
+	"fr" => "Configuration des VDS",
+	"en" => "Configuration of the VPSes",
+	"hu" => "TRANS Configuration of the VPSes",
+	"it" => "TRANS Configuration of the VPSes",
+	"nl" => "TRANS Configuration of the VPSes",
+	"ru" => "TRANS Configuration of the VPSes",
+	"de" => "TRANS Configuration of the VPSes",
+	"zh" => "TRANS Configuration of the VPSes",
+	"pl" => "TRANS Configuration of the VPSes",
+	"se" => "TRANS Configuration of the VPSes",
+	"es" => "TRANS Configuration of the VPSes",
+	"pt" => "TRANS Configuration of the VPSes"
+	);
+
+$txt_root_admin_no_product = array(
+	"fr" => "Pas de produit",
+	"en" => "No product",
+	"hu" => "TRANS No product",
+	"it" => "TRANS No product",
+	"nl" => "TRANS No product",
+	"ru" => "TRANS No product",
+	"de" => "TRANS No product",
+	"zh" => "TRANS No product",
+	"pl" => "TRANS No product",
+	"se" => "TRANS No product",
+	"es" => "TRANS No product",
+	"pt" => "TRANS No product"
+	);
+
+$txt_root_admin_add_a_vps_for_this_admin = array(
+	"fr" => "Ajouter un VPS à cet administrateur:",
+	"en" => "Add a VPS for this admin:",
+	"hu" => "TRANS Add a VPS for this admin:",
+	"it" => "TRANS Add a VPS for this admin:",
+	"nl" => "TRANS Add a VPS for this admin:",
+	"ru" => "TRANS Add a VPS for this admin:",
+	"de" => "TRANS Add a VPS for this admin:",
+	"zh" => "TRANS Add a VPS for this admin:",
+	"pl" => "TRANS Add a VPS for this admin:",
+	"se" => "TRANS Add a VPS for this admin:",
+	"es" => "TRANS Add a VPS for this admin:",
+	"pt" => "TRANS Add a VPS for this admin:"
+	);
+
+$txt_root_admin_vps_server_hostname = array(
+	"fr" => "Nom d'hote du serveur de VDS : ",
+	"en" => "VPS Server hostname: ",
+	"hu" => "TRANS VPS Server hostname: ",
+	"it" => "TRANS VPS Server hostname: ",
+	"nl" => "TRANS VPS Server hostname: ",
+	"ru" => "TRANS VPS Server hostname: ",
+	"de" => "TRANS VPS Server hostname: ",
+	"zh" => "TRANS VPS Server hostname: ",
+	"pl" => "TRANS VPS Server hostname: ",
+	"se" => "TRANS VPS Server hostname: ",
+	"es" => "TRANS VPS Server hostname: ",
+	"pt" => "TRANS VPS Server hostname: "
+	);
+
+$txt_root_admin_delete_one_of_the_admin_vps = array(
+	"fr" => "Effacer l'un des VPS d'una admin : ",
+	"en" => "Delete one of the admin VPS: ",
+	"hu" => "TRANS Delete one of the admin VPS: ",
+	"it" => "TRANS Delete one of the admin VPS: ",
+	"nl" => "TRANS Delete one of the admin VPS: ",
+	"ru" => "TRANS Delete one of the admin VPS: ",
+	"de" => "TRANS Delete one of the admin VPS: ",
+	"zh" => "TRANS Delete one of the admin VPS: ",
+	"pl" => "TRANS Delete one of the admin VPS: ",
+	"se" => "TRANS Delete one of the admin VPS: ",
+	"es" => "TRANS Delete one of the admin VPS: ",
+	"pt" => "TRANS Delete one of the admin VPS: "
+	);
+
+$txt_root_admin_to_add_a_vps_you_have_to = array(
+	"fr" => "Pour ajouter un VPS, vous devez provisionner des IPs VDS dans le paneau general config et faire des VDS dans les produits.",
+	"en" => "To add a VPS, you need to setup some free IPs VPS in the general config and setup some VPS products.",
+	"hu" => "TRANS To add a VPS, you need to setup some free IPs VPS in the general config and setup some VPS products.",
+	"it" => "TRANS To add a VPS, you need to setup some free IPs VPS in the general config and setup some VPS products.",
+	"nl" => "TRANS To add a VPS, you need to setup some free IPs VPS in the general config and setup some VPS products.",
+	"ru" => "TRANS To add a VPS, you need to setup some free IPs VPS in the general config and setup some VPS products.",
+	"de" => "TRANS To add a VPS, you need to setup some free IPs VPS in the general config and setup some VPS products.",
+	"zh" => "TRANS To add a VPS, you need to setup some free IPs VPS in the general config and setup some VPS products.",
+	"pl" => "TRANS To add a VPS, you need to setup some free IPs VPS in the general config and setup some VPS products.",
+	"se" => "TRANS To add a VPS, you need to setup some free IPs VPS in the general config and setup some VPS products.",
+	"es" => "TRANS To add a VPS, you need to setup some free IPs VPS in the general config and setup some VPS products.",
+	"pt" => "TRANS To add a VPS, you need to setup some free IPs VPS in the general config and setup some VPS products."
+	);
+
+$txt_root_admin_delete_one_of_the_admin_dedicated_server = array(
+	"fr" => "Effacer l'un des serveurs dédié de l'administrateur : ",
+	"en" => "Delete one of the admin dedicated server: ",
+	"hu" => "TRANS Delete one of the admin dedicated server: ",
+	"it" => "TRANS Delete one of the admin dedicated server: ",
+	"nl" => "TRANS Delete one of the admin dedicated server: ",
+	"ru" => "TRANS Delete one of the admin dedicated server: ",
+	"de" => "TRANS Delete one of the admin dedicated server: ",
+	"zh" => "TRANS Delete one of the admin dedicated server: ",
+	"pl" => "TRANS Delete one of the admin dedicated server: ",
+	"se" => "TRANS Delete one of the admin dedicated server: ",
+	"es" => "TRANS Delete one of the admin dedicated server: ",
+	"pt" => "TRANS Delete one of the admin dedicated server: "
+	);
+
+$txt_root_admin_add_a_dedicated_server_to_admin = array(
+	"fr" => "Ajoute un serveur dédié à cet administrateur:",
+	"en" => "Add a dedicated server for this admin:",
+	"hu" => "TRANS Add a dedicated server for this admin:",
+	"it" => "TRANS Add a dedicated server for this admin:",
+	"nl" => "TRANS Add a dedicated server for this admin:",
+	"ru" => "TRANS Add a dedicated server for this admin:",
+	"de" => "TRANS Add a dedicated server for this admin:",
+	"zh" => "TRANS Add a dedicated server for this admin:",
+	"pl" => "TRANS Add a dedicated server for this admin:",
+	"se" => "TRANS Add a dedicated server for this admin:",
+	"es" => "TRANS Add a dedicated server for this admin:",
+	"pt" => "TRANS Add a dedicated server for this admin:"
+	);
+
+$txt_root_admin_product = array(
+	"fr" => "Produit",
+	"en" => "Product",
+	"hu" => "TRANS Product",
+	"it" => "TRANS Product",
+	"nl" => "TRANS Product",
+	"ru" => "TRANS Product",
+	"de" => "TRANS Product",
+	"zh" => "TRANS Product",
+	"pl" => "TRANS Product",
+	"se" => "TRANS Product",
+	"es" => "TRANS Product",
+	"pt" => "TRANS Product"
+	);
+
+$txt_root_admin_hostname = array(
+	"fr" => "Nom d'hote : ",
+	"en" => "Hostname: ",
+	"hu" => "TRANS Hostname: ",
+	"it" => "TRANS Hostname: ",
+	"nl" => "TRANS Hostname: ",
+	"ru" => "TRANS Hostname: ",
+	"de" => "TRANS Hostname: ",
+	"zh" => "TRANS Hostname: ",
+	"pl" => "TRANS Hostname: ",
+	"se" => "TRANS Hostname: ",
+	"es" => "TRANS Hostname: ",
+	"pt" => "TRANS Hostname: "
+	);
+
+$txt_root_admin_country = array(
+	"fr" => "Pays : ",
+	"en" => "Country: ",
+	"hu" => "TRANS Country: ",
+	"it" => "TRANS Country: ",
+	"nl" => "TRANS Country: ",
+	"ru" => "TRANS Country: ",
+	"de" => "TRANS Country: ",
+	"zh" => "TRANS Country: ",
+	"pl" => "TRANS Country: ",
+	"se" => "TRANS Country: ",
+	"es" => "TRANS Country: ",
+	"pt" => "TRANS Country: "
+	);
+
+$txt_root_admin_domain_config_table_title = array(
+	"fr" => "Configuration des domaines",
+	"en" => "Configuration of the domains",
+	"hu" => "TRANS Configuration of the domains",
+	"it" => "TRANS Configuration of the domains",
+	"nl" => "TRANS Configuration of the domains",
+	"ru" => "TRANS Configuration of the domains",
+	"de" => "TRANS Configuration of the domains",
+	"zh" => "TRANS Configuration of the domains",
+	"pl" => "TRANS Configuration of the domains",
+	"se" => "TRANS Configuration of the domains",
+	"es" => "TRANS Configuration of the domains",
+	"pt" => "TRANS Configuration of the domains"
+	);
+
+$txt_root_admin_vps_server = array(
+	"fr" => "Serveur VPS",
+	"en" => "VPS Server",
+	"hu" => "TRANS VPS Server",
+	"it" => "TRANS VPS Server",
+	"nl" => "TRANS VPS Server",
+	"ru" => "TRANS VPS Server",
+	"de" => "TRANS VPS Server",
+	"zh" => "TRANS VPS Server",
+	"pl" => "TRANS VPS Server",
+	"se" => "TRANS VPS Server",
+	"es" => "TRANS VPS Server",
+	"pt" => "TRANS VPS Server"
+	);
+
+$txt_root_admin_vps_name = array(
+	"fr" => "Nom du VPS",
+	"en" => "VPS Name",
+	"hu" => "TRANS VPS Name",
+	"it" => "TRANS VPS Name",
+	"nl" => "TRANS VPS Name",
+	"ru" => "TRANS VPS Name",
+	"de" => "TRANS VPS Name",
+	"zh" => "TRANS VPS Name",
+	"pl" => "TRANS VPS Name",
+	"se" => "TRANS VPS Name",
+	"es" => "TRANS VPS Name",
+	"pt" => "TRANS VPS Name"
+	);
+
+$txt_root_admin_registration = array(
+	"fr" => "Enregistrement",
+	"en" => "Registration",
+	"hu" => "TRANS Registration",
+	"it" => "TRANS Registration",
+	"nl" => "TRANS Registration",
+	"ru" => "TRANS Registration",
+	"de" => "TRANS Registration",
+	"zh" => "TRANS Registration",
+	"pl" => "TRANS Registration",
+	"se" => "TRANS Registration",
+	"es" => "TRANS Registration",
+	"pt" => "TRANS Registration"
+	);
+
+$txt_root_admin_expiration = array(
+	"fr" => "Expiration",
+	"en" => "Expiration",
+	"hu" => "TRANS Expiration",
+	"it" => "TRANS Expiration",
+	"nl" => "TRANS Expiration",
+	"ru" => "TRANS Expiration",
+	"de" => "TRANS Expiration",
+	"zh" => "TRANS Expiration",
+	"pl" => "TRANS Expiration",
+	"se" => "TRANS Expiration",
+	"es" => "TRANS Expiration",
+	"pt" => "TRANS Expiration"
+	);
+
+$txt_root_admin_configuration_of_the_dedicated_servers = array(
+	"fr" => "Configuration des serveurs dédiés",
+	"en" => "Configuration of the dedicated servers",
+	"hu" => "TRANS Configuration of the dedicated servers",
+	"it" => "TRANS Configuration of the dedicated servers",
+	"nl" => "TRANS Configuration of the dedicated servers",
+	"ru" => "TRANS Configuration of the dedicated servers",
+	"de" => "TRANS Configuration of the dedicated servers",
+	"zh" => "TRANS Configuration of the dedicated servers",
+	"pl" => "TRANS Configuration of the dedicated servers",
+	"se" => "TRANS Configuration of the dedicated servers",
+	"es" => "TRANS Configuration of the dedicated servers",
+	"pt" => "TRANS Configuration of the dedicated servers"
+	);
+
+$txt_root_admin_country2 = array(
+	"fr" => "Pays",
+	"en" => "Country",
+	"hu" => "TRANS Country",
+	"it" => "TRANS Country",
+	"nl" => "TRANS Country",
+	"ru" => "TRANS Country",
+	"de" => "TRANS Country",
+	"zh" => "TRANS Country",
+	"pl" => "TRANS Country",
+	"se" => "TRANS Country",
+	"es" => "TRANS Country",
+	"pt" => "TRANS Country"
+	);
+
+$txt_root_admin_server_name = array(
+	"fr" => "Nom du serveur",
+	"en" => "Server name",
+	"hu" => "TRANS Server name",
+	"it" => "TRANS Server name",
+	"nl" => "TRANS Server name",
+	"ru" => "TRANS Server name",
+	"de" => "TRANS Server name",
+	"zh" => "TRANS Server name",
+	"pl" => "TRANS Server name",
+	"se" => "TRANS Server name",
+	"es" => "TRANS Server name",
+	"pt" => "TRANS Server name"
+	);
+
+$txt_root_admin_ = array(
+	"fr" => "",
+	"en" => "",
+	"hu" => "TRANS ",
+	"it" => "TRANS ",
+	"nl" => "TRANS ",
+	"ru" => "TRANS ",
+	"de" => "TRANS ",
+	"zh" => "TRANS ",
+	"pl" => "TRANS ",
+	"se" => "TRANS ",
+	"es" => "TRANS ",
+	"pt" => "TRANS "
+	);
+
+$txt_root_admin_ = array(
+	"fr" => "",
+	"en" => "",
+	"hu" => "TRANS ",
+	"it" => "TRANS ",
+	"nl" => "TRANS ",
+	"ru" => "TRANS ",
+	"de" => "TRANS ",
+	"zh" => "TRANS ",
+	"pl" => "TRANS ",
+	"se" => "TRANS ",
+	"es" => "TRANS ",
+	"pt" => "TRANS "
+	);
+
+$txt_root_admin_ = array(
+	"fr" => "",
+	"en" => "",
+	"hu" => "TRANS ",
+	"it" => "TRANS ",
+	"nl" => "TRANS ",
+	"ru" => "TRANS ",
+	"de" => "TRANS ",
+	"zh" => "TRANS ",
+	"pl" => "TRANS ",
+	"se" => "TRANS ",
+	"es" => "TRANS ",
+	"pt" => "TRANS "
+	);
+
+$txt_root_admin_ = array(
+	"fr" => "",
+	"en" => "",
+	"hu" => "TRANS ",
+	"it" => "TRANS ",
+	"nl" => "TRANS ",
+	"ru" => "TRANS ",
+	"de" => "TRANS ",
+	"zh" => "TRANS ",
+	"pl" => "TRANS ",
+	"se" => "TRANS ",
+	"es" => "TRANS ",
+	"pt" => "TRANS "
+	);
+
+$txt_root_admin_ = array(
+	"fr" => "",
+	"en" => "",
+	"hu" => "TRANS ",
+	"it" => "TRANS ",
+	"nl" => "TRANS ",
+	"ru" => "TRANS ",
+	"de" => "TRANS ",
+	"zh" => "TRANS ",
+	"pl" => "TRANS ",
+	"se" => "TRANS ",
+	"es" => "TRANS ",
+	"pt" => "TRANS "
+	);
+
+$txt_root_admin_ = array(
+	"fr" => "",
+	"en" => "",
+	"hu" => "TRANS ",
+	"it" => "TRANS ",
+	"nl" => "TRANS ",
+	"ru" => "TRANS ",
+	"de" => "TRANS ",
+	"zh" => "TRANS ",
+	"pl" => "TRANS ",
+	"se" => "TRANS ",
+	"es" => "TRANS ",
+	"pt" => "TRANS "
+	);
+
+?>
