952b63582feb9ae6b3816b959c5b5aa5ce2021a0
thomas <thomas>
2007-03-18 16:45:50 +0000

    Some unusefull code commented out.

--- admin/checkbind.sh
+++ admin/checkbind.sh
@@ -2,15 +2,17 @@
 conf_generated_file_path=$1
 
 bindgroup=`cat /etc/group | cut -f 1 -d: | grep named`
-nobodygroup=`cat /etc/group | cut -f 1 -d: | grep ^nobody`
+
+# That part is from old < 0.25 version, removing it...
+#nobodygroup=`cat /etc/group | cut -f 1 -d: | grep ^nobody`
 # if we can't find the nobody group, try nogroup
-if [ -z ""$nobodygroup ]; then
-nobodygroup=`cat /etc/group | cut -f 1 -d: | grep ^nogroup`
-fi
+#if [ -z ""$nobodygroup ]; then
+#	nobodygroup=`cat /etc/group | cut -f 1 -d: | grep ^nogroup`
+#fi
 # if we can't find nogroup, then set to 65534
-if [ -z ""$nobodygroup ]; then
-nobodygroup=65534
-fi
+#if [ -z ""$nobodygroup ]; then
+#	nobodygroup=65534
+#fi
 
 if [ -z "$bindgroup" ] ; then
 	bindgroup=`cat /etc/group | cut -f 1 -d: | grep bind`
