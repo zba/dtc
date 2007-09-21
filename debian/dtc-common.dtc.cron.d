71ae4f2ae61450a7437ea2d48423942a4f00f1e8
Thomas Goirand <thomas@goirand.fr>
2007-09-21 21:46:23 +0800

    Now redirects 2>&1 in the cron of DTC.

--- debian/dtc-common.dtc.cron.d
+++ debian/dtc-common.dtc.cron.d
@@ -1,6 +1,6 @@
 # /etc/cron.d/dtc
 
-*/10 * * * * root [ -d /usr/share/dtc/admin ] && cd /usr/share/dtc/admin && /usr/bin/php /usr/share/dtc/admin/cron.php >> /var/log/dtc.log
-9    4 * * * dtc  [ -d /usr/share/dtc/admin ] && cd /usr/share/dtc/admin && nice -n+20 /usr/bin/php /usr/share/dtc/admin/accesslog.php
-*    * * * * root [ -d /usr/share/dtc/admin ] && cd /usr/share/dtc/admin && nice -n+20 /usr/share/dtc/admin/rrdtool.sh >> /var/log/dtc.log
+*/10 * * * * root [ -d /usr/share/dtc/admin ] && cd /usr/share/dtc/admin && /usr/bin/php /usr/share/dtc/admin/cron.php >> /var/log/dtc.log 2>&1
+9    4 * * * dtc  [ -d /usr/share/dtc/admin ] && cd /usr/share/dtc/admin && nice -n+20 /usr/bin/php /usr/share/dtc/admin/accesslog.php 2>&1
+*    * * * * root [ -d /usr/share/dtc/admin ] && cd /usr/share/dtc/admin && nice -n+20 /usr/share/dtc/admin/rrdtool.sh >> /var/log/dtc.log 2>&1
 34   0 * * * root [ -d /usr/share/dtc/admin ] && cd /usr/share/dtc/admin && /usr/bin/php /usr/share/dtc/admin/reminders.php
