5d223db4348fb53778f9ae504af6b732e5940a0d
Thomas Goirand <thomas@goirand.fr>
2007-11-17 15:45:49 +0800

    VAT field was wrong: set as decimal 3,2 when decimal 4,2 at least is needed.

--- admin/tables/companies.sql
+++ admin/tables/companies.sql
@@ -5,7 +5,7 @@ CREATE TABLE IF NOT EXISTS companies (
   country varchar(128) NOT NULL default '',
   registration_number varchar(128) NOT NULL default '',
   vat_number varchar(128) NOT NULL default '',
-  vat_rate decimal(3,2) NOT NULL default '0.00',
+  vat_rate decimal(9,2) NOT NULL default '0.00',
   logo_path varchar(255) NOT NULL default 'none',
   text_after text NOT NULL,
   footer text NOT NULL,
