787663ceed7d71ad4b8f2f696d71f00e1da2fcec
tusker <tusker@packrat.datalexsin.local>
2008-02-05 14:41:26 +0800

    Fix dkfilter so that it signs outgoing mail correctly, but still validates and uses amavisd for incoming mail

--- admin/genfiles/gen_named_files.php
+++ admin/genfiles/gen_named_files.php
@@ -401,7 +401,7 @@ $more_mx_server
               $SELECTOR="postfix";
               $DOMAIN=$web_name;
               $NSRECORD="$SELECTOR._domainkey IN TXT \"k=rsa;p=$KEY; t=y\"";
-              $NSRECORDDEFAULT="_domainkey IN TXT \"k=rsa;p=$KEY; t=y\"";
+              $NSRECORDDEFAULT="_domainkey IN TXT \"o=~\"";
               $this_site_file .= "$NSRECORDDEFAULT\n";
               $this_site_file .= "$NSRECORD\n";
       }
