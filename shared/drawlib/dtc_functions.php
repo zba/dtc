7dbd73e2e2f3e9abb18e59849641fbbdf1d3e72f
Thomas Goirand <thomas@goirand.fr>
2008-03-26 21:54:13 +0800

    Added patch for the new version of dkimproxy.

--- shared/drawlib/dtc_functions.php
+++ shared/drawlib/dtc_functions.php
@@ -1,5 +1,48 @@
 <?php
 
+function domainNamePopup($domain_name=""){
+	$out = "";
+
+	$allTLD = array(".com", ".aero", ".asia", ".biz", ".cat", ".coop", ".edu", ".gov", ".info", ".int", ".jobs", ".mil", ".mobi", ".museum", ".name", ".net",".org", ".pro", ".tel", ".travel",
+".ac",".ad",".ae",".af",".ag",".ai",".al",".am",".an",".ao",".aq",".ar",".as",".at",".au",".aw",".ax",".az",
+".ba",".bb",".bd",".be",".bf",".bg",".bh",".bi",".bj",".bm",".bn",".bo",".br",".bs",".bt",".bw",".by",".bz",
+".ca",".cc",".cd",".cf",".cg",".ch",".ci",".ck",".cl",".cm",".cn",".co",".cr",".cu",".cv",".cx",".cy",".cz",
+".de",".dj",".dk",".dm",".do",".dz",
+".ec",".ee",".eg",".er",".es",".et",".eu",
+".fi",".fj",".fk",".fm",".fo",".fr",
+".ga",".gb",".gd",".ge",".gf",".gg",".gh",".gi",".gl",".gm",".gn",".gp",".gq",".gr",".gs",".gt",".gu",".gw",".gy",
+".hk",".hm",".hn",".hr",".ht",".hu",
+".id",".ie",".il",".im",".in",".io",".iq",".ir",".is",".it",
+".je",".jm",".jo",".jp",
+".ke",".kg",".kh",".ki",".km",".kn",".kp",".kr",".kw",".ky",".kz",
+".la",".lb",".lc",".li",".lk",".lr",".ls",".lt",".lu",".lv",".ly",
+".ma",".mc",".md",".me",".mg",".mh",".mk",".ml",".mm",".mn",".mo",".mp",".mq",".mr",".ms",".mt",".mu",".mv",".mw",".mx",".my",".mz",
+".na",".nc",".ne",".nf",".ng",".ni",".nl",".no",".np",".nr",".nu",".nz",".nc",
+".om",
+".pa",".pe",".pf",".pg",".ph",".pk",".pl",".pm",".pn",".pr",".ps",".pt",".pw",".py",
+".qa",
+".re",".ro",".rs",".ru",".rw",
+".sa",".sb",".sc",".sd",".se",".sg",".sh",".si",".sk",".sl",".sm",".sn",".sr",".st",".sv",".sy",".sz",
+".tc",".td",".tf",".tg",".th",".tj",".tk",".tl",".tm",".tn",".to",".tr",".tt",".tv",".tw",".tz",
+".ua",".ug",".ac.uk",".co.uk",".gov.uk",".ltd.uk",".me.uk",".mod.uk",".net.uk",".org.uk",".plc.uk",".sch.uk",".us",".uy",".uz",
+".va",".vc",".ve",".vg",".vi",".vn",".vu",
+".wf",".ws",
+".ye",".yt",".yu",
+".za",".zm",".zw");
+
+	$keys = array_keys($allTLD);
+	$nbr_tld = sizeof($allTLD);
+	for($i=0;$i<$nbr_tld;$i++){
+		if( ereg("\\".$keys[$i]."\$",$domain_name)){
+			$selected = " selected ";
+		}else{
+			$selected = "";
+		}
+		$out .= "<option value=\"".$keys[$i]."\" $selected>". $keys[$i] ."</option>";
+	}
+	return $out;
+}
+
 function findLastUsedLangByUser($adm_login){
 	global $pro_mysql_admin_table;
 	global $pro_mysql_new_admin_table;
