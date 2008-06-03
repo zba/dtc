469eaeb90cc33f3f2b6fd3e38b6b92dd631a789d
Thomas Goirand <thomas@goirand.fr>
2008-06-04 00:13:36 +0800

    Added the IP pool table.

---
+++ admin/tables/ip_pool.sql
@@ -0,0 +1,9 @@
+CREATE TABLE IF NOT EXISTS ip_pool (
+  `id` int(11) NOT NULL auto_increment,
+  `location` varchar(255) NOT NULL default '',
+  `ip_addr` varchar(16) NOT NULL default '',
+  `netmask` varchar(16) NOT NULL default '',
+  `zone_type` enum('support_ticket','ip_per_ip','one_zonefile') default 'one_zonefile',
+  PRIMARY KEY  (id),
+  UNIQUE KEY ip_addr (ip_addr)
+) TYPE=MyISAM;
