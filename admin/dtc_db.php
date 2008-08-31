9e4ef34e8c6748bb346ec651a9bc9a0d543d79b1
Thomas Goirand <thomas@goirand.fr>
2008-08-31 19:45:53 +0800

    Added RDNS entries to the dedicated_ip table.

--- admin/dtc_db.php
+++ admin/dtc_db.php
@@ -277,6 +277,20 @@ $dtc_database = array(
 			"server_hostname" => "(server_hostname)"
 			)
 		),
+	"dedicated_ip" => array(
+		"vars" => array (
+			"id" => "int(11) NOT NULL auto_increment",
+			"dedicated_server_hostname" => "varchar(255) NOT NULL default ''",
+			"ip_addr" => "varchar(16) NOT NULL default ''",
+			"available" => "enum('yes','no') NOT NULL default 'yes'",
+			"rdns_addr" => "varchar(255) NOT NULL default 'gplhost.com'",
+			"rdns_regen" => "enum('yes','no') NOT NULL default 'yes'"
+			),
+		"primary" => "(id)",
+		"unique" => array(
+				"ip_addr" => "(ip_addr)"
+                        )
+		),
 	"domain" => array(
 		"vars" => array(
 			"id" => "int(12) NOT NULL auto_increment",
