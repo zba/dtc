0f314f5f7607b32c9af5ad98435eb14766379cbb
Thomas Goirand <thomas@goirand.fr>
2009-03-12 05:56:55 +0800

    Patched a bit the work from Tabányi István

--- admin/dtc_db.php
+++ admin/dtc_db.php
@@ -258,6 +258,7 @@ $dtc_database = array(
 			"gen_vhosts" => "enum('yes','no') NOT NULL default 'no' ",
 			"gen_named" => "enum('yes','no') NOT NULL default 'no' ",
 			"gen_reverse" => "enum('yes','no') NOT NULL default 'no'",
+			"gen_fetchmail" => "enum('yes','no') NOT NULL default 'no'",
 			"gen_qmail" => "enum('yes','no') NOT NULL default 'no' ",
 			"gen_webalizer" => "enum('yes','no') NOT NULL default 'no' ",
 			"gen_backup" => "enum('yes','no') NOT NULL default 'no' ",
