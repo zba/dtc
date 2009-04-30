a488c9f7353dbc4521ac4be3dba470768d1ec676
Thomas Goirand <thomas@goirand.fr>
2009-04-30 16:45:29 +0800

    Adding the admin/support-receive.php file.

---
+++ admin/support-receive.php
@@ -0,0 +1,31 @@
+#!/usr/bin/env php
+<?php
+
+chdir(dirname(__FILE__));
+
+require("../shared/autoSQLconfig.php"); // Our main configuration file
+require_once("$dtcshared_path/dtc_lib.php");
+
+// This comes from the Mail_Mime PEAR package, under Debian, you need
+// the php-mail-mime package to have this script work.
+require_once 'Mail/mimeDecode.php';
+
+// Email header parsing
+function decodeEmail($input){
+	$params['include_bodies'] = true;
+	$params['decode_bodies']  = true;
+	$params['decode_headers'] = true;
+	$decoder = new Mail_mimeDecode($input);
+	$structure = $decoder->decode($params);
+	return $structure;
+}
+
+// $struct = decodeEmail($input);
+$fp = fopen('php://stdin', 'r');
+while($line = fgets($fp, 4096) ){
+  echo $line;
+}
+
+
+
+?>
