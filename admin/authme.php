d92449a39d1106adac085251abda8fc0f4f1fdbc
Thomas Goirand <thomas@goirand.fr>
2009-06-05 03:14:27 +0800

    Added the auth...

---
+++ admin/authme.php
@@ -0,0 +1,19 @@
+<?php
+
+if( !$_SERVER["PHP_AUTH_USER"] ){
+	Header( "WWW-authenticate: basic realm=\"DTC Admin ".$_SERVER["HTTP_HOST"]."\"" );
+	Header( "HTTP/1.0 401 Unauthorized" );
+	echo _("Please login with username and password in order to access the DTC admin interface.");
+	die();
+}else{
+	$q = "SELECT * FROM $pro_mysql_tik_admins_table WHERE pseudo='".mysql_escape_string($_SERVER['PHP_AUTH_USER'])."' AND tikadm_pass='".mysql_escape_string($_SERVER['PHP_AUTH_PW'])."';";
+	$r = mysql_query($q)or die("Cannot query for auth line ".__LINE__." file ".__FILE__);
+	$n = mysql_num_rows($r);
+	if($n != 1){
+		echo _("Wrong login or password.");
+		die();
+	}
+}
+
+
+?>
