e6f7372f8b72214435dc9aece6a579c494918ca1
Thomas Goirand <thomas@goirand.fr>
2010-02-04 02:10:05 +0800

    Added extplorer.htusers.php as example /etc/extplorer/.htusers.php in doc/contrib

---
+++ doc/contrib/extplorer.htusers.php
@@ -0,0 +1,30 @@
+<?php 
+
+function dtc_db_auth() {
+	if( !file_exists("/usr/share/dtc/shared/mysql_config.php") ){
+		return false;
+	}
+	require_once("/usr/share/dtc/shared/mysql_config.php");
+
+	$ressource_id = mysql_connect("$conf_mysql_host", "$conf_mysql_login", "$conf_mysql_pass");
+	if($ressource_id == false){
+		return false;
+	}
+	mysql_select_db($conf_mysql_db)or die("Could not connect to the DTC db!");
+	$q = "SELECT * FROM admin";
+	$r = mysql_query($q)or die("Could not query the DTC db!");
+	$n = mysql_num_rows($r);
+	for($i=0;$i<$n;$i++){
+		$a = mysql_fetch_array($r);
+		$GLOBALS["users"][] = array($a["adm_login"],md5($a["adm_pass"]),$a["path"],"http://localhost",1,"",7,1);
+	}
+}
+
+	/** ensure this file is being included by a parent file */
+	defined( "_VALID_MOS" ) or die( "Direct Access to this location is not allowed." );
+	$GLOBALS["users"]=array(
+	array("admin2","28d488ed5bd5b96fee0934441f350357","/var/www/sites/dtc/xen650901.gplhost.com/subdomains/dtc/html","http://localhost",1,"",7,1),
+
+); 
+	dtc_db_auth();
+?>
