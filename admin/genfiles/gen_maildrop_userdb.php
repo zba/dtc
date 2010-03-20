dd7bf6448fae9caecafbe74f5e0b03e6715ac3c7
Thomas Goirand <thomas@goirand.fr>
2010-03-20 19:10:58 +0800

    - cron.php: Add /usr/local/etc/authlib and /usr/local/etc/courier as possibles paths to userdb file - genfiles/gen_maildrop_userdb.php: Add /usr/local/sbin to possible path for makeuserdb (defaults to /usr/sbin) - genfiles/gen_maildrop_userdb.php: call makeuserdb with -f to path_userdb to accomodate different possible paths.

--- admin/genfiles/gen_maildrop_userdb.php
+++ admin/genfiles/gen_maildrop_userdb.php
@@ -35,6 +35,12 @@ function mail_account_generate_maildrop(){
 
 	if( file_exists("/etc/courier/userdb") ){
 		$path_userdb="/etc/courier/userdb";
+	}elseif( file_exists("/usr/local/etc/userdb") ){
+		$path_userdb="/usr/local/etc/userdb";
+	}elseif( file_exists("/usr/local/etc/authlib/userdb") ){
+		$path_userdb="/usr/local/etc/authlib/userdb";
+	}elseif( file_exists("/usr/local/etc/courier/userdb") ){
+		$path_userdb="/usr/local/etc/courier/userdb";
 	}else{
 		$path_userdb="/etc/authlib/userdb";
 	}
@@ -123,6 +129,11 @@ function mail_account_generate_maildrop(){
 
 	// Create the binary database
 	system("/usr/sbin/makeuserdb");
+	if( file_exists("/usr/local/sbin/makeuserdb") ){
+		system("/usr/local/sbin/makeuserdb -f " . $path_userdb);
+	}else{
+		system("/usr/sbin/makeuserdb -f " . $path_userdb);
+	}
 	return;
 }
 
