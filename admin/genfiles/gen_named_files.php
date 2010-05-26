1624fc10afdb20b2d960d6140da9da28c3f39977
Thomas Goirand <thomas@goirand.fr>
2010-05-26 11:22:22 +0800

    Added details about failed parking domains in the error message.

--- admin/genfiles/gen_named_files.php
+++ admin/genfiles/gen_named_files.php
@@ -917,7 +917,7 @@ function named_generate(){
 			$rp = mysql_query($qp)or die("Cannot query $qp line ".__LINE__." file ".__FILE__." sql said ".mysql_error());
 			$np = mysql_num_rows($rp);
 			if($np != 1){
-				echo "WARNING: error in your SQL table: target domain does not exists, will cancel domain parking!!!";
+				echo "WARNING: error in your SQL table: target domain $domain_parking for parking of $web_name does not exists, will cancel domain parking!!!<br>";
 				$domain_to_get = $web_name;
 				$domain_parking = "no-parking";
 			}else{
