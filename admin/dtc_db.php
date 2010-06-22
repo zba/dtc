0dcf4c07951a28727144a109c0d33d2a2fcde5b1
Thomas Goirand <thomas@goirand.fr>
2010-06-22 15:35:27 +0800

    Killed the http_accounting table repair code at setup time, as it is reseting bandwidth on each upgrade.

--- admin/dtc_db.php
+++ admin/dtc_db.php
@@ -604,27 +604,6 @@ $dtc_database = array(
 			"name" => "(name,owner)"
 			)
 		),
-	"http_accounting" => array(
-		"vars" => array(
-			"id" => "int(14) NOT NULL auto_increment",
-			"vhost" => "varchar(50) NOT NULL ",
-			"bytes_sent" => "bigint(14) unsigned NOT NULL default '0' ",
-			"bytes_receive" => "bigint(14) unsigned NOT NULL default '0' ",
-			"count_hosts" => "int(12) NOT NULL default '0' ",
-			"count_visits" => "int(12) NOT NULL default '0' ",
-			"count_status_200" => "int(12) NOT NULL default '0' ",
-			"count_status_404" => "int(12) NOT NULL default '0' ",
-			"count_impressions" => "int(18) NOT NULL default '0' ",
-			"last_run" => "int(14) NOT NULL default '0' ",
-			"month" => "int(4) NOT NULL default '0' ",
-			"year" => "int(4) NOT NULL default '0' ",
-			"domain" => "varchar(50) NOT NULL "
-			),
-		"primary" => "(id)",
-                "unique" => array(
-                	"vhost" => "(vhost,month,year,domain)"
-			)
-		),
 	"ip_pool" => array (
 		"vars" => array(
 			"id" => "int(11) NOT NULL auto_increment",
