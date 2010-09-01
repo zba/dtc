841c1862a654cbfddfa7a58c73c21575039206e6
Fernando Kiernan <fkiernan@id.net.ar>
2010-09-01 13:13:10 -0300

    - Fix squirrelmail and phpmyadmin on FreeBSD.

--- admin/genfiles/gen_pro_vhost.php
+++ admin/genfiles/gen_pro_vhost.php
@@ -650,8 +650,12 @@ AND $pro_mysql_admin_table.id_client != '0'";
 				vhost_chk_dir_sh("$web_path/$web_name/subdomains/$web_subname/html");
 				vhost_chk_dir_sh("$web_path/$web_name/subdomains/$web_subname/logs");
 				vhost_chk_dir_sh("$web_path/$web_name/subdomains/$web_subname/cgi-bin");
-				$vhost_file .= "	Alias /phpmyadmin ".$conf_tools_prefix."/phpmyadmin
-	Alias /dtc $conf_dtcclient_path
+				if ( $conf_unix_type == "bsd" ) {
+					$vhost_file .= "	Alias /phpmyadmin ".$conf_tools_prefix."/phpMyAdmin\n";
+				}else{
+					$vhost_file .= "	Alias /phpmyadmin ".$conf_tools_prefix."/phpmyadmin\n";
+				}
+				$vhost_file .= "	Alias /dtc $conf_dtcclient_path
 	Alias /dtcdoc $conf_dtcdoc_path/html/en
 	Alias /dtcemail $conf_dtcemail_path
 	Alias /dtcadmin $conf_dtcadmin_path/
